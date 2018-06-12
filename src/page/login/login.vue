a1628603a4b842e58e448ac3ae22b034298e7cf8
田博 <15712406167@163.com>
2018-06-12 16:40:04 +0800

    tianbo

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -410,8 +410,12 @@
 
 <style lang="scss" scoped>
   .app{
+    position:absolute;
+    top:0;
   width:100%;
+  height:100%;
   overflow-x:hidden;
+  background:#FFF;
 }
   .top{
     position: fixed;
@@ -707,15 +711,8 @@ position: absolute;
     float:right;
     left:30%;
     bottom:62px;
-    // width:100px;
     height:40px;
     text-align:center;
-    // text-align: center;
-    // height:60%;
-    // bottom:30%;
-    // bottom:30%;
-    // top:-40px;
-    // margin-left:30px;
    
   }
  .email{
