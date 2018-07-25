41d69f02a605dc8a11af75ca97c843599d74151d
liuying <942792546@qq.com>
2018-07-25 14:15:00 +0800

    标签列表

--- src/page/contentInfo/labelList/labelList.vue
+++ src/page/contentInfo/labelList/labelList.vue
@@ -195,7 +195,7 @@ let id = 1000;
   .add{width: 450px; height: 250px;margin: auto;position: fixed; top:50%; left:50%; margin-top:-125px; margin-left:-225px; background: #fff; overflow: hidden; border:1px solid #eeeeee; border-radius: 3px;}
   .label{ background: #f6f6f6; border-bottom: 1px solid #eee;height: 40px; line-height: 40px; width: 450px;}
   .left{ float: left; margin: 0 0 0 10px; }
-  .right{float: right; margin: 0 10px 0 0;font-size: 20pt;cursor:pointer; }
+  .right{float: right; margin: 0 10px 0 0;font-size: 16pt;cursor:pointer; }
   .content{ width: 310px; overflow: hidden;margin-left:auto;margin-right:auto;}
   .according{margin: 50px 0 0 0;margin-left:auto;margin-right:auto;}
   .text{float: left; line-height: 40px;}
