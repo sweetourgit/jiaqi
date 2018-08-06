7f3f3a981ea07f5809ece2248f639a81adb5e3f1
田博 <15712406167@163.com>
2018-08-06 18:11:24 +0800

    ew

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@ var vm = new Vue({
 vm.$router.beforeEach((to,from,next) => {
     if(to.matched.some( m => m.meta.auth)){
         // 对路由进行验证
-        if(sessionStorage.getItem('userId')) { // 已经登陆
+        if(sessionStorage.getItem('userId')) {  // 已经登陆
             next()     // 正常跳转到你设置好的页面
         }else{
             // 未登录则跳转到登陆界面，query:{ Rurl: to.fullPath}表示把当前路由信息传递过去方便登录后跳转回来；
