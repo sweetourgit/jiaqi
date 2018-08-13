ea9c3565c33005aa0c2c0fd72728f4c9bef17ba8
tanghao <363310279@qq.com>
2018-08-13 16:14:26 +0800

    productlist

--- src/page/productManagement/productList/productList.vue
+++ src/page/productManagement/productList/productList.vue
@@ -1,9 +1,238 @@
 <template>
-  <router-link to="listInfo"><el-button type="primary" >添加产品</el-button></router-link>
+  <div>
+    <div class="select_button">
+      <el-button plain>跟团游</el-button>
+      <el-button plain>自由行</el-button>
+      <el-button plain>签证</el-button>
+      <el-button plain>机票</el-button>
+      <el-button plain>酒店</el-button>
+      <el-button plain>门票</el-button>
+      <el-button plain>WiFi</el-button>
+      <el-button plain>电话卡</el-button>
+      <el-button plain>交通</el-button>
+      <el-button plain>保险</el-button>
+      <router-link to="listInfo"><el-button type="primary" style="margin-left: 10px">添加产品</el-button></router-link>
+    </div>
+    <div style="height: 155px;">
+      <div class="select_two">
+      <div class="id">ID <el-input style="width: 200px; margin-left: 10px;"  placeholder="请输入内容"></el-input></div>
+      <div class="product">商品名称 <el-input style="width: 200px; margin-left: 10px;"  placeholder="请输入内容"></el-input></div>
+      <div class="options">产品操作人 <el-input style="width: 200px; margin-left: 10px;"  placeholder="请输入内容"></el-input></div>
+      <div class="prices">价格<el-input style="width: 100px; margin-left: 10px;"  placeholder="请输入内容"></el-input><span style="margin-left: 10px">——</span><el-input style="width: 100px; margin-left: 10px;"  placeholder="请输入内容"></el-input></div>
+      </div>
+      <div class="select_two">
+        <div class="address">出发地 <el-input style="width: 200px; margin-left: 10px;"  placeholder="请输入内容"></el-input></div>
+        <div class="name">商品名称 <el-input style="width: 200px; margin-left: 10px;"  placeholder="请输入内容"></el-input></div>
+        <div class="options11">状态
+          <el-select  style="margin-left: 33px;width: 200px;" v-model="value" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
+        </div>
+      </div>
+      <div class="select_two_button">
+        <el-button type="primary" size="medium">搜索</el-button>
+        <el-button type="primary" size="medium">重置</el-button>
+      </div>
+
+    </div>
+    <div class="table_trip">
+      <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+        <el-table-column
+          prop="pid"
+          label="产品编号"
+         >
+        </el-table-column>
+        <el-table-column
+          prop="type"
+          label="类型"
+         >
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="产品名称"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="mu_address"
+          label="目的地"
+         >
+        </el-table-column>
+        <el-table-column
+          prop="options"
+          label="操作人"
+         >
+        </el-table-column>
+        <el-table-column
+          label="状态"
+          >
+          <template slot-scope="scope">
+          <el-button type="success"  size="mini">已上架</el-button>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="opers"
+          label="推送平台"
+        width="200">
+        </el-table-column>
+        <el-table-column
+          prop="price"
+          label="价格">
+        </el-table-column>
+        <el-table-column
+          label="操作"
+        width="300">
+          <template slot-scope="scope">
+            <div>
+            <el-button type="primary" size="mini" @click="handleClick(scope.row)">编辑</el-button>
+            <el-button type="primary" size="mini">复制</el-button>
+            <el-button type="primary" size="mini">导出行程</el-button>
+            </div>
+            <div style="margin-top: 10px">
+            <el-button type="primary" size="mini">退改</el-button>
+            <el-button type="primary" size="mini">团期/库存</el-button>
+            <el-button type="danger" size="mini">删除</el-button>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <div class="block">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage4"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400">
+        </el-pagination>
+      </div>
+    </div>
+
+  </div>
+
 </template>
 <script>
-
+  export default {
+    data() {
+      return {
+        currentPage1: 5,
+        currentPage2: 5,
+        currentPage3: 5,
+        currentPage4: 4,
+        options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value: '',
+        tableData: [{
+          pid:'1',
+          type:'跟团游',
+          name:'xxx 跟团游',
+          mu_address:'xxx',
+          options:'xxx',
+          status:'1',
+          opers:'飞猪 携程',
+          price:'7900'
+        }]
+      }
+    },
+    methods: {
+      handleClick(row) {
+        console.log(row);
+      },
+      handleSizeChange(val) {
+        console.log(`每页 ${val} 条`);
+      },
+      handleCurrentChange(val) {
+        console.log(`当前页: ${val}`);
+      }
+    },
+  }
 </script>
 <style scoped>
-
+ .select_button{
+   width: 1200px;
+   margin-left: -70px;
+ }
+  .id{
+    width: 250px;
+    float: left;
+    font-size: 14px;
+    margin-left: 11px;
+    padding-left: 7px;
+  }
+ .address{
+   width: 260px;
+   float: left;
+   font-size: 14px;
+ }
+  .select_two{
+    height: 45px;
+    margin-top: 10px;
+    margin-left: 30px;
+  }
+ .product{
+   width: 300px;
+   float: left;
+   margin-left: 10px;
+   font-size: 14px;
+ }
+  .options{
+    width: 320px;
+    float: left;
+    margin-left: 10px;
+    font-size: 14px;
+  }
+ .options11{
+   width: 320px;
+   float: left;
+   margin-left: 10px;
+   font-size: 14px;
+ }
+  .name{
+    width: 320px;
+    float: left;
+    margin-left: 8px;
+    font-size: 14px;
+  }
+  .prices{
+    width: 320px;
+    float: left;
+    font-size: 14px;
+  }
+  .select_two_button{
+    height: 45px;
+    margin-top: 10px;
+    margin-left: 30px;
+    float: left;
+  }
+  .table_trip{
+  }
+ .block{
+   float: right;
+   margin-top: 50px;
+   margin-bottom: 80px;
+ }
 </style>
