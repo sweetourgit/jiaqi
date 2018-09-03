cda734e1c58da5e2f7c24e24ae894b4c0843b7d8
田博 <15712406167@163.com>
2018-09-03 15:09:48 +0800

    productList.vue

--- src/page/productManagement/productList/productList.vue
+++ src/page/productManagement/productList/productList.vue
@@ -78,7 +78,7 @@
       <el-button plain>复制</el-button>
       <el-button plain>导出行程</el-button>
       <el-button plain>退改</el-button>
-      <el-button plain>团期/库存</el-button>
+      <el-button plain @click = "groupStage">团期/库存</el-button>
       <el-button type="danger" plain>删除</el-button>
     </div>
 
@@ -168,7 +168,13 @@
     <div v-if="isCollapse==true">
 
       <div class="button-list" >
-        <el-button :id="'kk' + index" class="property" plain v-for="(data, index) in buttonList" :key=data.id   @click="begin(data, index)">{{data.button}}</el-button>
+        <template v-if="this.forbidden == true">
+          <el-button :id="'kk' + index" class="property" plain v-for="(data, index) in buttonList" :key=data.id   @click="begin(data, index)">{{data.button}}</el-button>         
+        </template>
+        <template v-else>
+          <el-button :id="'kk' + index" class="property" plain v-for="(data, index) in buttonList" :key=data.id   @click="begin(data, index)" disabled>{{data.button}}</el-button>          
+        </template>
+        
       </div>
       <!-- 其实这个表格遍历数据的时候是有问题的,因为目前没有数据,每次遍历的都是addtable里面的数据,每次点击库存都会添加一条,这个地方应该是点击库存的时候传一个id按照这个id遍历数据 -->
       <el-table
@@ -184,7 +190,7 @@
           label="属性"
           width="230"
           align="center"
-
+          
           >
         </el-table-column>
         <el-table-column
@@ -192,30 +198,30 @@
           label="值"
           align="center"
          >
-
+         
         <template slot-scope="scope" prop="ll">
               <template v-if="scope.row.ll == ''">
                 <el-button @click="skuadd()" type="primary" v-show="addsku" size="mini" style="float:left">生成sku</el-button>
-              </template>
-           <template  slot-scope="scope" v-else>
+              </template>         
+           <template  slot-scope="scope" v-else>           
               <div v-for="(data,index) in addtable[addtable.length-1].allprice[scope.$index].value"  :key="data.id" v-show="aa" style="float:left">
-                  <el-input  id="rr" style="width:100px;" v-model="addtable[addtable.length-1].allprice[scope.$index].value[index].price"  type="text" ></el-input>
+                  <el-input style="width:100px;" v-model="addtable[addtable.length-1].allprice[scope.$index].value[index].price"  type="text" clearable></el-input>
                  <!-- v-model="addtable[scope.$index].price[index].value" -->
                  <!-- v-model="addtable[addtable.length-1].allpricep[scope.$index].value" -->
-                  <el-button  style="margin-right:10px;" size="mini" type="danger">删除</el-button>
+                  <el-button  style="margin-right:10px;" size="mini" type="danger" @click="shanchu(index,scope.$index)">删除</el-button>
               </div>
-                <el-button :id="'vv' + index" plain v-show="bb" v-for="(data,index) in addtable[addtable.length-1].allprice[scope.$index].value"
+                <el-button :id="'vv' + index" plain v-show="bb" v-for="(data,index) in addtable[addtable.length-1].allprice[scope.$index].value"  
                 :key="data.id" style="float:left;margin-right:10px;"  type="primary"
                 @click="choice(data, index,addtable[addtable.length-1].allprice[scope.$index])"
                 >
                   {{addtable[addtable.length-1].allprice[scope.$index].value[index].price}}
                 </el-button>
-
+            
             </template>
         </template>
-
+         
          <!-- 生成sku的按钮 -->
-
+        
         </el-table-column>
         <el-table-column label="操作"
           width="300"
@@ -225,12 +231,12 @@
           <template v-if="scope.row.ll == ''">
             <el-button v-show="qq" @click="gain" size="mini" type="primary">确认属性值</el-button>
              <!--重新设置属性  -->
-            <el-button v-show="again" type="danger" @click="back" size="mini">重新设置属性</el-button>
+            <el-button v-show="again" type="danger" @click="back" size="mini">重新设置属性</el-button>           
           </template>
           <template v-else>
             <el-button v-show="pp" @click="addInput(scope.row,scope.$index)" size="mini" type="primary" >添加值</el-button>
-            <el-button v-show="close"  size="mini" type="primary"  disabled>添加值</el-button>
-
+            <el-button v-show="close"  size="mini" type="primary"  disabled>添加值</el-button>            
+                        
           </template>
         </template>
 
@@ -241,9 +247,9 @@
 
       <el-table
        v-show="skuList"
-
+      
         :data="ccc"
-
+        
         border
          style="width: 1340px;margin:30px auto;"
         :header-cell-style="getRowClass">
@@ -261,7 +267,7 @@
         align="center"
         >
 
-
+          
       </el-table-column>
 
       <el-table-column
@@ -273,6 +279,16 @@
         prop="name"
         align="center"
         label="出行模板">
+        <template slot-scope="scope">
+          <el-select v-model="value" placeholder="请选择">
+            <el-option
+              v-for="item in type"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+      </template>
       </el-table-column>
         <el-table-column
         prop="name"
@@ -350,11 +366,15 @@ import DateList from './component/DateList'
         currentPage2: 5,
         currentPage3: 5,
         currentPage4: 4,
+
         merchandise: false,
         isCollapse: true,
         aaa:0,
+       
+        // 属性按钮禁用
+        forbidden:true,
         // 属性按钮选中效果
-        mm:true,
+        mm:true, 
         // 添加值按钮
         pp:true,
         // 确认属性值按钮
@@ -392,25 +412,25 @@ import DateList from './component/DateList'
           id: "1",
           button: "行程路线",
           pp : false,
-          verifier: "Route",
+          verifier: "Route",  
         },
         {
           id: "2",
           button: "天数",
           pp : false,
-          verifier: "Day",
+          verifier: "Day", 
         },
         {
           id: "3",
           button: "晚数",
           pp : false,
-          verifier: "NightNum",
+          verifier: "NightNum",      
         },
         {
           id: "4",
           button: "房型",
           pp : false,
-          verifier: "House",
+          verifier: "House",         
         },
         {
           id: "5",
@@ -422,7 +442,7 @@ import DateList from './component/DateList'
           id: "6",
           button: "航空公司",
           pp : false,
-          verifier: "Airline",
+          verifier: "Airline",        
         },
         {
           id: "7",
@@ -459,6 +479,22 @@ import DateList from './component/DateList'
         }, {
           value: '选项5',
           label: '北京烤鸭'
+        }],
+       type: [{
+          value: '1',
+          label: '国内出行'
+        }, {
+          value: '2',
+          label: '境外出行'
+        }, {
+          value: '3',
+          label: '周边游'
+        }, {
+          value: '4',
+          label: '城市玩乐'
+        }, {
+          value: '5',
+          label: '参团游'
         }],
         value: '',
         tableData: [{
@@ -533,12 +569,14 @@ import DateList from './component/DateList'
       }
     },
 
+   // 点击属性按钮的时候
     begin(e,key){
-
+   
       // 如果按钮是未按下的情况下
       if(e.pp == false){
         document.getElementById('kk'+key).style.border = 'solid 1px #409EFF'
         document.getElementById('kk'+key).style.color= '#409EFF'
+        this.forbidden == true
         // 点击向数组中添加数值
         this.addtable[this.addtable.length-1].allprice.push({
           id:e.id,
@@ -559,10 +597,10 @@ import DateList from './component/DateList'
           // 如果数据条数大于一条,则吧str,也就是确认属性值这条代码拿出来在重新放到数据尾部
           var str = this.addtable[this.addtable.length-1].allprice.splice( this.addtable[this.addtable.length-1].allprice.length -2,1);
           this.addtable[this.addtable.length -1].allprice.push(str[0]);
-          this.buttonList[key].key = this.addtable[this.addtable.length -1].allprice.length -2;
-          console.log(e);
+          this.buttonList[key].key = this.addtable[this.addtable.length -1].allprice.length -2; 
+          console.log(e);         
         }
-
+        
       }else if(e.pp){
         // 当按钮按下了
         document.getElementById('kk'+key).style.border = 'solid 1px #dcdfe6'
@@ -570,77 +608,27 @@ import DateList from './component/DateList'
         console.log("e.id:"+ e.id);
         console.log("e.key:" + e.key);
         console.log(this.addtable[this.addtable.length-1].allprice);
-        // console.log(this.addtable[this.addtable.length-1].allprice[e.key].id);
         // 这个地方删除有些问题!
         this.addtable[this.addtable.length -1].allprice.splice(e.key,1);
-        // console.log(this.addtable[this.addtable.length-1].allprice);
         for(let i=0;i<this.addtable[this.addtable.length-1].allprice.length -1;i++){
-          // console.log(i);
           this.buttonList[this.addtable[this.addtable.length-1].allprice[i].id].key = i;
         }
         if(this.addtable[this.addtable.length-1].allprice.length ==1){
           this.addtable[this.addtable.length-1].allprice.splice(0,1);
         }
         this.buttonList[key].pp = false;
-      }
-    // console.log(this.addtable);
-
+      }      
     },
     //添加属性值功能
     addInput(b,key){
       this.aa = true;
-      console.log(b);
+      // console.log(b);
       // 向指定的属性数组中添加属性值
       this.addtable[this.addtable.length-1].allprice[key].value.push({
         price:'',
       })
-      // console.log(this.addtable[this.addtable.length-1].allprice[key].value);
-
-      // console.log(this.addtable[this.addtable.length-1].allprice[key].value.price);
-
-      // console.log(this.addtable);
     },
-
-
-
-  // 点击添加按钮方法
-  // addInput(b, key){
-
-  //   this.aa = true;
-  //   this.aaa += 1;
-
-  //   this.addtable[key].price.push({
-  //     key: this.aaa-1,
-
-  //     value:'',
-
-  //   });
-
-  //   console.log(this.sku);
-  //   console.log(b);
-  //   console.log(this.addtable[key].price[this.addtable[key].price.length - 1].value);
-  //   console.log(this.addtable[key].price.length);
-  // this.key = tt;
-  //    console.log(this.addtable);
-
-  //   },
-
-    searchEnterFun:function(e){
-        this.addtable[e].price[this.price.length - 1].str.push({
-          oo:this.price.str,
-        })
-              // this.price.push({
-              //   qq:'',
-              //   str:this.price.str,
-              // })
-              // this.price.splice(this.price.length-1,0,this.price.str);
-              console.log(this.price.length);
-              console.log(this.price.str);
-              console.log(this.price);
-              this.aa = false;
-
-              this.bb = true;
-            },
+  
 
   // 确认属性值
   gain(){
@@ -653,50 +641,24 @@ import DateList from './component/DateList'
     this.pp = false;
     this.close = true;
     this.addsku = true;
+    // 属性按钮被禁用
+    this.forbidden = false;
 
-  //  console.log(this.addtable);
-
-  // for(var q=0;q<this.addtable.length-1;q++){
-  //     this.sku.push({
-  //     verifier:this.addtable[q].verifier,
-  //     value:[],
-  //   });
-  //   console.log(this.addtable[q].price);
-  //   for(var w=0;w<this.addtable[q].price.length;w++){
-  //     console.log(this.addtable[q].price[w].value);
-  //     console.log(this.sku.length);
-  //     for(var r=0;r<this.sku.length;r++){
-  //       this.sku[r].value.push({
-  //         price:this.addtable[q].price[w].value,
-  //       })
-  //     console.log(this.addtable[q].price[w].value);
-
-  //     }
-  //   }
-  // }
-    // console.log(this.addtable)
-    // console.log(this.addtable.length-1);
-
-    // console.log(this.addtable.price.value);
-    // this.sku.push({
-    //  key: Date.now(),
-
-    // })
-    // console.log(this.sku);
-
-    // console.log(addtable[scope.$index].price.value);
-    // console.log(this.model);
+    console.log(this.addtable);
+    console.log(this.sku);
   },
-
+  
   // 重新设计属性值
   back(){
-    this.aa = true;
+    this.aa = true;    
     this.bb = false;
     this.qq = true;
     this.again = false;
     this.pp = true;
     this.close = false;
     this.addsku = false;
+    // 属性恢复使用
+    this.forbidden = true;
   },
 
   // 属性值按钮按下
@@ -713,51 +675,62 @@ import DateList from './component/DateList'
     //   document.getElementById('vv'+key).style.border = 'solid 1px #409EFF';
     //   document.getElementById('vv'+key).style.color= '#fff';
     //   document.getElementById('vv'+key).style.background= '#409EFF';
-
+  
     // }else{
     //   document.getElementById('vv'+key).style.border = 'solid 1px #b3d8ff';
     //   document.getElementById('vv'+key).style.color= '#409EFF';
     //   document.getElementById('vv'+key).style.background= '#409EFF';
-
+    
     // }
   },
   // 生成sku
   skuadd(){
-    console.log(this.sku);
-    this.skuList = true;
-    // var bbb = [
+    console.log(this.sku);  
+    
 
-    // ];
-    //  var bbb = [].concat(this.sku[this.sku.length-1]);
-    //  console.log(bbb);
-
-    // 获取点击按钮后的数据
+     console.log( this.sku[this.sku.length-1].price.length);
+     if(this.sku[this.sku.length-1].price.length==0){
+      this.$message({
+          showClose: true,
+          message: '请选择要添加的属性',
+          type: 'error'
+        });
+     }else{
+       // 获取点击按钮后的数据
+       this.skuList = true;
     var bbb = [];
     for(var i = 0;i<this.sku[this.sku.length-1].price.length;i++){
       bbb[i] = this.sku[this.sku.length-1].price[i]
     }
      this.sku[this.sku.length-1].price.splice(0,this.sku[this.sku.length-1].price.length);
-
-    // console.log(this.sku[this.sku.length-1].price);
     console.log(this.sku);
     console.log(bbb);
-    var ooo = []
-
+    var ooo = [];
   for(var k = 0;k<bbb.length;k++){
     ooo.push(
-         bbb[k].name + ':' + bbb[k].zhi
+         bbb[k].name + ':' + bbb[k].zhi     
     )
-
-
+    
+     
   }
-var ppp = ooo.toString()
+    var ppp = ooo.toString()
     // sku的id编号
     this.skuid++;
     this.ccc.push({
       id:this.skuid,
       ddd:ppp,
     })
-console.log(this.ccc)
+    console.log(this.ccc)
+     }
+    
+  },
+
+  // 删除属性值
+  shanchu(index,key){
+    console.log(this.addtable[this.addtable.length-1].allprice[key].value[index]);
+    // splice(this.addtable[this.addtable.length-1].allprice[index].value[index],0);
+    this.addtable[this.addtable.length-1].allprice[key].value.splice(index,1);
+
   }
 
 
