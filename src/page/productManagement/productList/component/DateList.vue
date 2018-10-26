e14d761237ad662e5fffc31d8ad64a92f4b196c8
田博 <15712406167@163.com>
2018-10-26 13:33:27 +0800

    DateList.vue

--- src/page/productManagement/productList/component/DateList.vue
+++ src/page/productManagement/productList/component/DateList.vue
@@ -1,5 +1,15 @@
 <template>
-  <div id="calendar">
+  <div>
+    <div class="leftSku" >
+      <div style="font-size:20px">Sku</div>
+        <el-button class="selectSku" plain v-for="item in ccc" :key="item.id">{{item.ddd}}</el-button>
+      <div style="font-size:20px;margin-top:20px">附加增值服务</div>
+      <el-button class="selectSku" plain v-for="item in Addprice" >{{item.name}}</el-button>
+
+      
+    </div>
+    <div id="calendar" >
+    
     <!-- 年份 月份 -->
     <div class="month">
         <ul class="date">
@@ -44,7 +54,7 @@
           <!--今天  同年同月同日-->
                 <span v-if="dayobject.day.getFullYear() == new Date().getFullYear() && dayobject.day.getMonth() == new Date().getMonth() && dayobject.day.getDate() == new Date().getDate()" class="active">{{ dayobject.day.getDate() }}</span>
                 <span v-else>{{ dayobject.day.getDate() }}</span>
-             <div class='person' v-show="dayobject.data.person.price">
+                <div class='person' v-show="dayobject.data.person.price">
                   <p class='old'>成人</p>
                   <p>结算价：{{dayobject.data.person.price}}</p>
                   <p>售出/余位：0/{{dayobject.data.person.number}}</p>
@@ -56,30 +66,86 @@
             <!---->
         </li>
     </ul>
-    <el-card class="box-card" v-show="n.length">
-  <div style="text-align: left;" slot="header" class="clearfix">
-    成人
-    
-    <el-button style="float:right" type="danger" size="mini">删除</el-button>
-    <el-button @click="handlesaveclick" style="float:right;margin-right:5px" type="primary" size="mini">保存</el-button>
-  </div>
-  <div>
-    <el-form ref="form" :model="form" label-width="80px">
-      <el-form-item label="结算价">
-        <el-input :maxlength='6' type='tel' v-model="form.price"></el-input>
-      </el-form-item>
-      <el-form-item label="剩余量">
-        <el-input :maxlength='6' v-model="form.number"></el-input>
-      </el-form-item>
-       <el-form-item label="购买下限">
-        <el-input :maxlength='6' v-model="form.down"></el-input>
-      </el-form-item>
-       <el-form-item label="购买上限">
-        <el-input :maxlength='6' v-model="form.top"></el-input>
-      </el-form-item>
-    </el-form>
-  </div>
-</el-card>
+    <!-- 右侧的表单 -->
+    <div class="rightForm" v-show="rightTable">
+      <!-- 表单 -->
+      <el-form :model="Rform">
+          <el-form-item label="报名类型:">
+              <el-select v-model="Rform.region" placeholder="请选择" style="width:150px" @change="AddType">
+                <el-option v-for="item in typeSelect" 
+                :label="item.label"
+                :value="item.value"
+                :key="item.value"></el-option>
+              </el-select>
+            <el-button size="mini" type="primary" >添加</el-button>
+          </el-form-item>
+          <el-form-item label="库存类型:" prop="resource">
+              <el-radio-group v-model="Rform.resource" @change="xuanze">
+                <el-radio label="共享" value="0"></el-radio>
+                <el-radio label="非公享" value="1"></el-radio>
+              </el-radio-group>
+          </el-form-item>
+          <!-- 共享库存 -->
+          <el-form-item label="共享库存:" v-if='repertorySelect == "share"'  style="margin-top:-15px;">
+              <el-select v-model="Rform.shareRepertory" placeholder="请选择" style="width:150px">
+                <el-option v-for="item in typeSelect" 
+                :label="item.label"
+                :value="item.value"
+                :key="item.value"></el-option>
+              </el-select>
+              <div><span>当前剩余:</span>&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:blue">90</span></div>
+          </el-form-item>
+          <!-- 非共享库存 -->
+          <el-form-item label="总库存:" v-if='repertorySelect == "sum"' style="margin-top:-15px;" >
+              <el-input v-model="Rform.sumRepertory" style="width:200px;" ></el-input>
+            </el-form-item>
+      </el-form>
+      <!-- 卡片 -->
+      <template>
+        <el-card class="box-card"  v-for="(item,index) in arr" :key="item.key">
+          <!-- v-for="card in n[0].AddType"  -->
+          <!-- v-if='n[0]!=Addtype'  -->
+        <div  slot="header" class="clearfix">
+          {{item.name}}
+        <div style="float:right;margin-top: -3px;">
+          <el-button style="" type="primary" size="mini" @click="">保存</el-button>
+          
+          <el-button @click="delect(index)"  type="danger" size="mini">删除</el-button>
+          <template v-if="arr[index].quota == false">
+            <el-button @click="AddQuota(index)"  type="primary" size="mini">添加配额</el-button>
+          </template>
+          <template v-else>
+            <el-button @click="DelectQuota(index)"  type="primary" size="mini">删除配额</el-button>            
+          </template>
+          
+        </div>
+         
+        </div>
+   
+        <div>
+          <el-form ref="form"  label-width="80px">
+            <el-form-item label="销售价">
+              <el-input :maxlength='6' type='tel' v-model="item.salePrice"></el-input>
+            </el-form-item>
+            <el-form-item label="同业价">
+              <el-input :maxlength='6' v-model="item.traderPrice"></el-input>
+            </el-form-item>
+            <el-form-item label="配额" v-if="arr[index].quota == true">
+              <el-input :maxlength='6' v-model="item.quotaPrice"></el-input>
+            </el-form-item>
+              <!-- <el-form-item label="购买下限">
+              <el-input :maxlength='6' v-model="form.down"></el-input>
+            </el-form-item>
+              <el-form-item label="购买上限">
+              <el-input :maxlength='6' v-model="form.top"></el-input>
+            </el-form-item> -->
+          </el-form>
+        </div>
+      </el-card>
+      </template>
+        
+    </div>
+
   <!-- <el-card class="box-card2" v-show="n.length">
   <div style="text-align: left;" slot="header" class="clearfix">
     单价房
@@ -100,6 +166,8 @@
   </div>
 </el-card> -->
 </div>
+  </div>
+  
 </template>
 
 <script>
@@ -115,6 +183,15 @@ export default {
     return {
       today: new Date(),
       showCard: false,
+      // 大表单
+      Rform:{
+        region:'',
+        resource:'',
+        // 共享库存
+        shareRepertory:'',
+        // 总库存
+        sumRepertory:'',
+      },
       form: {
         price: "",
         number: "",
@@ -133,11 +210,20 @@ export default {
       sat: false,
       sun: false,
       n: [],
+      // 配额
+      quota:false,
+      // 日期信息
+      // DayMessage:[], 
       currentDay: 1,
       currentMonth: 1,
       currentYear: 1970,
       currentWeek: 1,
+      // 库存选择
+      repertorySelect:"",
+      // 右侧的选择表格
+      rightTable:false,
       days: [],
+      arr:[],
       leftobj: [
         //存放剩余数量
         { count: 1 },
@@ -145,7 +231,71 @@ export default {
         { count: 3 },
         { count: 4 },
         { count: 5 }
-      ]
+      ],
+      // 添加报名类型
+      // addType:[],
+      // 选择报名类型时候传的数值
+      typeNum:'',
+      // 卡片隐藏
+      aaa:false,
+      // sku选择
+      ccc:[{
+          id:"1",
+          ddd:"普吉岛情侣",
+          type:false,
+          // value:"1",
+        },{
+          id:"2",
+          ddd:"普吉岛亲子",
+          type:false,
+          // value:"2",         
+        },{
+          id:"3",
+          ddd:"哈尔滨3天自由行",
+          type:false,
+          // value:"3",          
+        }],
+
+      // 附加增值服务
+      Addprice:[{
+           id:1,
+            name:"保险",
+            priceSelect:"非日历价格",
+            explain:"保护安全",
+            type:false,
+      },{
+           id:2,
+            name:"小费",
+            priceSelect:"日历价格",
+            explain:"给小费鼓励一下吧",
+            type:false,
+      }],
+      // 类型选择
+      typeSelect: [{
+          value: '0',
+          label: '成人',
+          name:'adult '
+        }, {
+          value: '1',
+          label: '儿童',
+          name:'child'
+        }, {
+          value: '2',
+          label: '老人',
+          name:'oldman'
+        }, {
+          value: '3',
+          label: '婴儿',
+          name:'infant'
+        }, {
+          value: '4',
+          label: '特殊人群',
+          name:'specialCrowd'
+        },{
+          value: '5',
+          label: '单房差',
+          name:'singleCavity'
+        }],
     };
   },
   watch: {
@@ -302,6 +452,8 @@ export default {
   },
   methods: {
     handleChildrenSave() {
+      console.log(this.n);
+      console.log(this.n.length);
       this.n.forEach(item => {
       item.data.person = {
           price: this.form2.price,
@@ -310,7 +462,7 @@ export default {
       });
        this.clearchecked();
     },
-    handlesaveclick() {
+    addQuota() {
       this.n.forEach(item => {
         item.data.person = {
           price: this.form.price,
@@ -382,6 +534,7 @@ export default {
     },
     handleitemclick(day) {
       console.log(day);
+      
       if (day.day.getMonth() + 1 === this.currentMonth) {
         if (day.day.getTime() < this.today.getTime() - 24 * 60 * 60 * 1000) {
           this.$message({
@@ -391,8 +544,14 @@ export default {
         } else {
           if (this.n.includes(day)) {
             this.n = this.n.filter(v => v != day);
+            console.log("删除日期");
+            this.rightTable = false;
           } else {
+            console.log("添加日期");
             this.n.push(day);
+            console.log(this.n);
+            console.log(this.n[this.n.length-1])
+            this.rightTable = true;
           }
         }
       } else {
@@ -545,6 +704,67 @@ export default {
       var d = day;
       if (d < 10) d = "0" + d;
       return y + "-" + m + "-" + d;
+    },
+    // 选择库存类型
+    xuanze(a){
+      if(a == "共享"){
+        console.log("哈哈哈");
+        // this.share = true;
+        // this.amount = false;
+        this.repertorySelect = "share";
+      }else if(a == "非公享"){
+        console.log("呜呜呜");
+        this.repertorySelect = "sum";
+
+        // this.amount = true;
+        // this.share = false;
+      }
+    },
+    // 添加报名类型
+    AddType(type){
+      this.aaa = true;
+      console.log(this.n);
+      
+      this.arr.push({
+        id:this.typeSelect[type].value,
+        name:this.typeSelect[type].label,
+        // 销售价
+        salePrice:'',
+        // 同业价
+        traderPrice:'',
+        quota:false,
+        quotaPrice:'',
+        
+      })
+      console.log(this.arr);
+      for(var ok=0;ok<this.n.length;ok++){
+        this.n[ok]["AddType"] = this.arr;
+        
+      }
+      // this.n["AddType"]=[];
+    //  this.n[0]["AddType"] = {[this.typeSelect[type].value]:this.typeSelect[type].label,[this.typeSelect[type].value]:this.typeSelect[type].label,[this.typeSelect[type].value]:this.typeSelect[type].label}
+
+    // var AddType = [];
+    // for(var ok=0;ok<this.n.length;ok++){
+
+    // }
+
+      console.log(this.n);
+    
+    },
+    // 删除卡片
+    delect(index){
+      this.arr.splice(index,1);
+      console.log(this.arr);
+    },
+    // 添加配额
+    AddQuota(index){
+      console.log(index);
+      this.arr[index].quota = true;
+    },
+    // 删除配额
+    DelectQuota(index){
+      this.arr[index].quota = false;
     }
   }
 };
@@ -608,10 +828,10 @@ body {
   padding: 10px;
 }
 .box-card {
-  position: absolute;
-  right: -340px;
+  /* position: absolute; */
+  /* right: -340px; */
   width: 339px;
-  top: 0;
+  /* top: 170px; */
 }
 .box-card2 {
   position: absolute;
@@ -719,5 +939,31 @@ body {
 }
 .days li:hover {
   background: #e1e1e1;
+}
+.leftSku{
+  width:300px;
+  height:500px;
+  float:left;
+  margin-top:40px;
+  margin-left:40px;
+  /* border:1px solid red; */
+}
+.selectSku{
+  margin-top:10px;
+  margin-left:40px;
+  width:200px;
+}
+.rightForm{
+  border:1px solid red;
+  position: absolute;
+  right: -340px;
+  width: 339px;
+  /* height:500px; */
+  top: 0;
+}
+.clearfix{
+  width:310px;
+  text-align: left;
+  /* border:1px solid blue;  */
 }
 </style>
