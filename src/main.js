8981b3968961a7a011369c832d1ac04b5b056c03
tanghao <tanghaolll@163.com>
2019-04-22 17:53:28 +0800

    12312

--- src/main.js
+++ src/main.js
@@ -7,8 +7,7 @@ import '../static/UE/ueditor.config.js'
 import '../static/UE/ueditor.all.min.js'
 import '../static/UE/lang/zh-cn/zh-cn.js'
 import '../static/UE/ueditor.parse.min.js'
-import "../dist/static/z-tree/jquery.ztree.core.js";
-import "../dist/static/z-tree/jquery.ztree.excheck.js";
+
 
 // element-ui
 import ElementUI from 'element-ui'
