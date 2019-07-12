f7ba03c13eabc2f276eaaf6d6eec72b8f9993fcd
331450663 <331450663@qq.com>
2019-07-12 17:12:09 +0800

    0

--- src/page/Finance/predepositManagement/predepositList.vue
+++ src/page/Finance/predepositManagement/predepositList.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-     111
+     <div class="demo-input-suffix">
+         <span class="search-title">客商名称</span>
+         <el-input placeholder="请输入" v-model="customerName" class="el-input"></el-input>
+         <span class="search-title">销售</span>
+         <el-input placeholder="请输入" v-model="saler" class="el-input"></el-input>
+         <el-button type="primary">搜索</el-button>
+         <el-button type="primary">重置</el-button>
+     </div>
+     <el-button type="primary" class="add" plain>添加</el-button>
   </div>
 </template>
 
@@ -12,7 +20,8 @@ export default {
   },
   data() {
     return {
-
+      customerName:'',
+      saler:'',
     }
   },
   created(){
@@ -26,5 +35,9 @@ export default {
 </script>
 
 <style scoped>
-
+    .demo-input-suffix{width: 900px}
+    .el-input{width:250px;margin-right: 10px}
+    .search-title{font-size: 14px;margin:0 10px}
+    .search-but{margin:20px 10px;}
+    .add{margin:30px 10px}
 </style>
