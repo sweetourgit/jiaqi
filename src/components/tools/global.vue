94e8cb41f873b98545c521ea943ed5e32b35783e
sunzhiwei <252177320@qq.com>
2019-07-26 09:36:10 +0800

    团期计划-对接添加接口

--- src/components/tools/global.vue
+++ src/components/tools/global.vue
@@ -2,6 +2,7 @@
   const serverSrc="http://192.168.1.186:3017";
   const imgUrl="http://192.168.1.186:3009";
   const jqUrl="http://192.168.1.198:6019";
+  const serverSrcPhp="http://www.around.com";
   const token="";
   const userId="";
   const hasEnter=false;
@@ -10,6 +11,7 @@
   serverSrc,//服务器地址
   imgUrl,//图片服务器地址
   jqUrl,//工作流启动地址
+  serverSrcPhp,//服务器地址:PHP相关接口
   token,//用户token身份
   userId,//用户id
   hasEnter,//用户登录状态
