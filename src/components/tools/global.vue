fd5e02cd3a47380c20818a33e8aea4f6618b2b02
zhangyi643 <18341314421@163.com>
2019-08-19 11:28:39 +0800

    修改服务地址

--- src/components/tools/global.vue
+++ src/components/tools/global.vue
@@ -3,10 +3,10 @@
   const imgUrl="http://192.168.2.4:3009";
   //const jqUrl="http://192.168.1.198:6019";
   const jqUrl="http://192.168.2.6:6019";
-  const serverSrcPhp="http://www.around.com";
+  const serverSrcPhp="http://192.168.2.17";
   const token="";
   const userId="";
-  const hasEnter=false; 
+  const hasEnter=false;
   export default {
   serverSrc,//服务器地址
   imgUrl,//图片服务器地址
