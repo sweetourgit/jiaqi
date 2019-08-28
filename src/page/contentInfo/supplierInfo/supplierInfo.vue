1d617adfc3e74ac5e14fa0b36a84d1d0af61a04d
liuying2018 <942792546@qq.com>
2019-08-28 09:32:41 +0800

    12

--- src/page/contentInfo/supplierInfo/supplierInfo.vue
+++ src/page/contentInfo/supplierInfo/supplierInfo.vue
@@ -82,7 +82,6 @@
             <el-input class="name_input" v-model="ruleForm.name"></el-input>
           </el-form-item>
           <el-form-item label="公司可见性" prop="visible">
-            <!-- <el-cascader :options="visibleType" v-model="ruleForm.visible" clearable></el-cascader> -->
             <el-select v-model="ruleForm.visible" placeholder="请选择">
               <el-option v-for="item in visibleType" :key="item.value":label="item.label":value="item.value"></el-option>
             </el-select>
@@ -108,7 +107,7 @@
             </el-select>
           </el-form-item>
           <el-form-item label="使用部门" prop="userDepartment">
-            <el-cascader  v-model="ruleForm.userDepartment" :props="props" clearable></el-cascader>
+            <el-cascader :disabled="ruleForm.visible != ''" v-model="ruleForm.userDepartment" :props="props" clearable></el-cascader>
           </el-form-item>
           <el-form-item label="产品主要方向" prop="orientation">
             <el-input class="name_input" v-model="ruleForm.orientation"></el-input>
@@ -201,34 +200,34 @@
     data(){
       return{
         aaa: 1,
-props: {
-  lazy: true,
-  _this: this,
-  multiple: true, checkStrictly: true,
-  lazyLoad(node, resolve) {
-    const { level } = node;
-    let nId = 204;
-    if (level > 0) {
-      nId = node.value;
-    }
-    console.log(nId);
-    this._this.$http
-    .post(this._this.GLOBAL.serverSrc + "/org/api/deptlist", {
-      object: {
-        ParentID: nId
-      }
-    })
-    .then(res => {
-    let data = res.data.objects.map(v => {
-      return {
-        label: v.orgName,
-        value: v.id
-      };
-    });
-    resolve(data);
-    });
-  }
-},
+        props: {
+          lazy: true,
+          _this: this,
+          multiple: true, checkStrictly: true,
+          lazyLoad(node, resolve) {
+            const { level } = node;
+            let nId = 204;
+            if (level > 0) {
+              nId = node.value;
+            }
+            console.log(nId);
+            this._this.$http
+            .post(this._this.GLOBAL.serverSrc + "/org/api/deptlist", {
+              object: {
+                ParentID: nId
+              }
+            })
+            .then(res => {
+            let data = res.data.objects.map(v => {
+              return {
+                label: v.orgName,
+                value: v.id,
+              };
+            });
+            resolve(data);
+            });
+          }
+        },
         supplierName:'',//搜索框供应商名称
         supplierCard:'',//搜索框ID
         settlement:'',//搜索框结算名称
@@ -311,201 +310,7 @@ props: {
         pathType:[],//添加弹窗中线路数组
         tableData:[],//表格
         supplierShow:false,//添加弹窗
-        branch: [{//使用部门选择器
-          value: 'zhinan',
-          label: '指南',
-          children: [{
-            value: 'shejiyuanze',
-            label: '设计原则',
-            children: [{
-              value: 'yizhi',
-              label: '一致'
-            }, {
-              value: 'fankui',
-              label: '反馈'
-            }, {
-              value: 'xiaolv',
-              label: '效率'
-            }, {
-              value: 'kekong',
-              label: '可控'
-            }]
-          }, {
-            value: 'daohang',
-            label: '导航',
-            children: [{
-              value: 'cexiangdaohang',
-              label: '侧向导航'
-            }, {
-              value: 'dingbudaohang',
-              label: '顶部导航'
-            }]
-          }]
-        }, {
-          value: 'zujian',
-          label: '组件',
-          children: [{
-            value: 'basic',
-            label: 'Basic',
-            children: [{
-              value: 'layout',
-              label: 'Layout 布局'
-            }, {
-              value: 'color',
-              label: 'Color 色彩'
-            }, {
-              value: 'typography',
-              label: 'Typography 字体'
-            }, {
-              value: 'icon',
-              label: 'Icon 图标'
-            }, {
-              value: 'button',
-              label: 'Button 按钮'
-            }]
-          }, {
-            value: 'form',
-            label: 'Form',
-            children: [{
-              value: 'radio',
-              label: 'Radio 单选框'
-            }, {
-              value: 'checkbox',
-              label: 'Checkbox 多选框'
-            }, {
-              value: 'input',
-              label: 'Input 输入框'
-            }, {
-              value: 'input-number',
-              label: 'InputNumber 计数器'
-            }, {
-              value: 'select',
-              label: 'Select 选择器'
-            }, {
-              value: 'cascader',
-              label: 'Cascader 级联选择器'
-            }, {
-              value: 'switch',
-              label: 'Switch 开关'
-            }, {
-              value: 'slider',
-              label: 'Slider 滑块'
-            }, {
-              value: 'time-picker',
-              label: 'TimePicker 时间选择器'
-            }, {
-              value: 'date-picker',
-              label: 'DatePicker 日期选择器'
-            }, {
-              value: 'datetime-picker',
-              label: 'DateTimePicker 日期时间选择器'
-            }, {
-              value: 'upload',
-              label: 'Upload 上传'
-            }, {
-              value: 'rate',
-              label: 'Rate 评分'
-            }, {
-              value: 'form',
-              label: 'Form 表单'
-            }]
-          }, {
-            value: 'data',
-            label: 'Data',
-            children: [{
-              value: 'table',
-              label: 'Table 表格'
-            }, {
-              value: 'tag',
-              label: 'Tag 标签'
-            }, {
-              value: 'progress',
-              label: 'Progress 进度条'
-            }, {
-              value: 'tree',
-              label: 'Tree 树形控件'
-            }, {
-              value: 'pagination',
-              label: 'Pagination 分页'
-            }, {
-              value: 'badge',
-              label: 'Badge 标记'
-            }]
-          }, {
-            value: 'notice',
-            label: 'Notice',
-            children: [{
-              value: 'alert',
-              label: 'Alert 警告'
-            }, {
-              value: 'loading',
-              label: 'Loading 加载'
-            }, {
-              value: 'message',
-              label: 'Message 消息提示'
-            }, {
-              value: 'message-box',
-              label: 'MessageBox 弹框'
-            }, {
-              value: 'notification',
-              label: 'Notification 通知'
-            }]
-          }, {
-            value: 'navigation',
-            label: 'Navigation',
-            children: [{
-              value: 'menu',
-              label: 'NavMenu 导航菜单'
-            }, {
-              value: 'tabs',
-              label: 'Tabs 标签页'
-            }, {
-              value: 'breadcrumb',
-              label: 'Breadcrumb 面包屑'
-            }, {
-              value: 'dropdown',
-              label: 'Dropdown 下拉菜单'
-            }, {
-              value: 'steps',
-              label: 'Steps 步骤条'
-            }]
-          }, {
-            value: 'others',
-            label: 'Others',
-            children: [{
-              value: 'dialog',
-              label: 'Dialog 对话框'
-            }, {
-              value: 'tooltip',
-              label: 'Tooltip 文字提示'
-            }, {
-              value: 'popover',
-              label: 'Popover 弹出框'
-            }, {
-              value: 'card',
-              label: 'Card 卡片'
-            }, {
-              value: 'carousel',
-              label: 'Carousel 走马灯'
-            }, {
-              value: 'collapse',
-              label: 'Collapse 折叠面板'
-            }]
-          }]
-        }, {
-          value: 'ziyuan',
-          label: '资源',
-          children: [{
-            value: 'axure',
-            label: 'Axure Components'
-          }, {
-            value: 'sketch',
-            label: 'Sketch Templates'
-          }, {
-            value: 'jiaohu',
-            label: '组件交互文档'
-          }]
-        }],
+        branch: [],//使用部门选择器
 
         fileList: [],//添加供应商上传附件
         agreement:[{//供应商协议
@@ -614,36 +419,6 @@ props: {
           console.log(err);
         })
       },
-      /*ccc(){
-        props: {
-          lazy: true,
-          _this: this,
-          lazyLoad(node, resolve) {
-          const { level } = node;
-          let nId = 204;
-          if (level > 0) {
-            nId = node.value;
-          }
-          console.log(nId);
-          this._this.$http
-          .post(this._this.GLOBAL.serverSrc + "/org/api/deptlist", {
-            object: {
-              ParentID: nId
-            }
-          })
-          .then(res => {
-          let data = res.data.objects.map(v => {
-            return {
-              label: v.orgName,
-              value: v.id
-            };
-          });
-          resolve(data);
-          });
-          // }, 100);
-          }
-}
-      },*/
       saveModule(formName){ //判断显示编辑或者添加弹窗
          if(this.title == "添加供应商"){
             this.addLabelTheme(formName);
@@ -756,11 +531,7 @@ props: {
       this.settlemen();//结算方式
       this.visible();//可见区域
       this.trails();//线路
-      this.ccc();
     },
-
-
- 
   }
 </script>
 
