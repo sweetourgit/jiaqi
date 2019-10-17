13012bd6f177b56f126651bac50c3f8609a47f1d
foolseward <foolseward@163.com>
2019-10-17 09:27:37 +0800

    config

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/app/17mall',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
