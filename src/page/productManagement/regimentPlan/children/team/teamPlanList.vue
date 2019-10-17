68b953e4c903e33b9eb4ac31a8826cec6c8302c2
liuying2018 <942792546@qq.com>
2019-10-17 17:45:25 +0800

    更新一版到测试

--- src/page/productManagement/regimentPlan/children/team/teamPlanList.vue
+++ src/page/productManagement/regimentPlan/children/team/teamPlanList.vue
@@ -11,7 +11,7 @@
         align="right" class="group-no" @change="dateline()"></el-date-picker>
       <br />
       <span class="search-title">操作人员</span>
-      <el-input placeholder="请输入" v-model="op" class="group-no" @blur="operation()"></el-input>
+      <el-input placeholder="请输入" v-model="op" class="group-no" @blur="operation_01()"></el-input>
       <span class="search-title">报账状态</span>
       <el-select v-model="financeState" placeholder="请选择" class="group-no" style="width:185px"@blur="condition()">
         <el-option v-for="item in financeType" :key="item.value" :label="item.label":value="item.value"></el-option>
@@ -28,17 +28,29 @@
       <!--列表表格-->
       <el-table :data="teamqueryList" ref="multipleTable" class="table" :header-cell-style="getRowClass" border :row-style="rowClass"
         :cell-style="getCellClass" @selection-change="changeFun" @row-click="clickRow">
-        <el-table-column type="index" label="序号" width="60"></el-table-column>
-        <el-table-column prop="groupCode" label="团号" width="220"></el-table-column>
-        <el-table-column prop="" label="状态" width="100"></el-table-column>
+        <!-- <el-table-column type="index" label="序号" width="60"></el-table-column> -->
+        <el-table-column prop="groupCode" label="团期计划" width="220"></el-table-column>
+        <el-table-column prop="regimentType" label="状态" width="100">
+          <template slot-scope="scope">
+            <div v-if="scope.row.regimentType=='1'">正常</div>
+            <div v-if="scope.row.regimentType=='2'">停售</div>
+            <div v-if="scope.row.regimentType=='3'">封团</div>
+            <div v-if="scope.row.regimentType=='4'">暂满</div>
+            <div v-if="scope.row.regimentType=='5'">满员</div>
+          </template>
+        </el-table-column>
         <el-table-column prop="title" label="产品名称" min-width="200"></el-table-column>
         <el-table-column prop="dateFormat" label="出行日期" width="100"></el-table-column>
-        <el-table-column prop="" label="报账状态" width="100"></el-table-column>
+        <el-table-column prop="accountStatus" label="报账状态" width="100">
+          <template slot-scope="scope">
+            <div>报账中</div>
+          </template>
+        </el-table-column>
         <el-table-column prop="count" label="计划位" width="70"></el-table-column>
         <el-table-column prop="remaining" label="余位" width="70"></el-table-column>
-        <el-table-column prop="" label="确定占位" width="70"></el-table-column>
-        <el-table-column prop="" label="预订占位" width="70"></el-table-column>
-        <el-table-column prop="" label="预订不占" width="70"></el-table-column>
+        <el-table-column prop="occupancy" label="确定占位" width="70"></el-table-column>
+        <el-table-column prop="scheduled" label="预订占位" width="70"></el-table-column>
+        <el-table-column prop="occupy" label="预订不占" width="70"></el-table-column>
         <!-- <el-table-column prop="week" label="周" width="70"></el-table-column>
         <el-table-column prop="day" label="天数" width="70"></el-table-column>
         <el-table-column prop="refPrice" label="参考价" width="80"></el-table-column>
@@ -46,6 +58,7 @@
         <el-table-column prop="remaining" label="余位" width="70"></el-table-column>
         <el-table-column prop="shareCN" label="是否共享" width="85"></el-table-column> -->
         <el-table-column prop="op" label="操作人员" width="80"></el-table-column>
+
         <el-table-column label="操作" width="180">
           <template slot-scope="scope">
             <span class="cursor blue" @click="haltSales(scope.row.id)">停售</span>
@@ -53,8 +66,8 @@
             <span class="cursor blue" @click="operation(1)">下单</span>
             <span class="em">|</span>
             <span class="cursor blue" @click="operation(2)">详情</span>
-            <span class="em">|</span>
-            <span class="cursor blue">报账单</span>
+            <!-- <span class="em">|</span>
+            <span class="cursor blue">报账单</span> -->
           </template>
         </el-table-column>
       </el-table>
@@ -94,6 +107,7 @@ export default {
   },
   data() {
     return {
+      userCodeList: null, // 获取UserList列表
       pageshow: true,
       planId: 0,
       variable: 0, //设置一个变量展示弹窗
@@ -102,6 +116,7 @@ export default {
       groupCode: "", //团号
       date: "", //日期
       op: "", //操作人员
+      changedUserCode: '', // 用户名转变
       financeState: "", //报账状态
       financeType: [
         {
@@ -150,11 +165,14 @@ export default {
         }
       ],
       dialogCost: false, //成本弹窗
-      costSelection: [] //选中的list
+      costSelection: [], //选中的list
+      searchParams: 2 // 2 为翻页，控制名字转code
     };
   },
   created() {
-    this.teamQueryList();
+    this.teamQueryList();    
+  },
+  mounted () {
     this.loadOper();
   },
   methods: {
@@ -173,7 +191,7 @@ export default {
         this.teamQueryList();
       }
     },
-    operation() {
+    operation_01() {
       if (this.op == "") {
         this.teamQueryList();
       }
@@ -214,13 +232,13 @@ export default {
     handleSizeChange(val) {
       this.pageSize = val;
       this.pageIndex = 1;
-      this.teamQueryList(1, val);
+      this.teamQueryList(1, val, this.searchParam);
     },
     handleCurrentChange(val) {
-      this.teamQueryList(val, this.pageSize);
+      this.teamQueryList(val, this.pageSize, this.searchParam);
     },
     //计划list
-    teamQueryList(pageIndex = this.pageIndex,pageSize = this.pageSize,title = this.title,groupCode = this.groupCode,startDate = this.date == null ? 0 : this.date[0],endDate = this.date == null ? 0 : this.date[1],op = this.op) {
+    teamQueryList(pageIndex = this.pageIndex,pageSize = this.pageSize,title = this.title,groupCode = this.groupCode,startDate = this.date == null ? 0 : this.date[0],endDate = this.date == null ? 0 : this.date[1],op = this.op,searchParams= this.searchParam) {
       if (startDate) {
         let y = startDate.getFullYear();
         let m = startDate.getMonth() + 1 > 9 ? startDate.getMonth() + 1 : "0" + (startDate.getMonth() + 1);
@@ -237,8 +255,27 @@ export default {
       } else {
         endDate = 0;
       }
-      this.$http
-        .post(this.GLOBAL.serverSrc + "/teamquery/get/api/page", {
+
+      let _this = this
+      console.log('searchParams', searchParams)
+      if (searchParams == 1) {
+        // 用户名转编码
+        if (this.userCodeList) {
+          try {
+              this.userCodeList.forEach(( item => {
+                console.log(item.name === _this.op)
+                if(item.name === _this.op) {
+                  _this.changedUserCode = item.userCode
+                  throw Error();
+                } else {
+                  _this.changedUserCode = ' ' 
+                }
+              }))  
+          } catch (e) {}
+        }
+      } 
+
+      this.$http.post(this.GLOBAL.serverSrc + "/teamquery/get/api/page", {
           pageIndex: pageIndex,
           pageSize: pageSize,
           object: {
@@ -247,27 +284,24 @@ export default {
             startDate: startDate,
             endDate: endDate,
             //"op":sessionStorage.getItem('userName')
-            op: op
+            op: _this.changedUserCode,
           }
         })
         .then(res => {
-          // console.log(res, 1);
           this.teamqueryList = [];
           this.total = res.data.total;
-          this.teamqueryList.forEach(function(item, index, arr) {
-            that.$http
-              .post(that.GLOBAL.serverSrc + "/org/api/userget", {
-                userCode: item.op
-              })
-              .then(function(response) {
-                if (response.data.isSuccess) {
-                  item.op = response.data.object.name;
-                }
-              })
-              .catch(function(error) {
-                console.log(error);
-              });
-          });
+          // this.teamqueryList.forEach(function(item, index, arr) {
+          //   that.$http.post(that.GLOBAL.serverSrc + "/org/api/userget", {
+          //       userCode: item.op
+          //   }).then(function(response) {
+          //       if (response.data.isSuccess) {
+          //         item.op = response.data.object.name;
+          //       }
+          //     })
+          //     .catch(function(error) {
+          //       console.log(error);
+          //     });
+          // });
           if (res.data.isSuccess == true) {
             this.teamqueryList = res.data.objects;
           }
@@ -311,10 +345,11 @@ export default {
       this.dialogType = i;
     },
     search() {
+      this.searchParam = 1;
       //this.pageIndex = 1;
       //this.pageshow = false;
       console.log(this.date);
-      this.teamQueryList();
+      this.teamQueryList(this.searchParam);
       this.$nextTick(() => {
         this.pageshow = true;
       });
@@ -347,9 +382,8 @@ export default {
         });
     },
     loadOper() {
-      const that = this;
-      this.$http
-        .post(
+      var that = this;
+      this.$http.post(
           this.GLOBAL.serverSrc + "/org/api/userlist",
           {
             object: {
@@ -381,25 +415,11 @@ export default {
                 }
               ]
             }
-          },
-          {
-            headers: {
-              Authorization: "Bearer " + localStorage.getItem("token")
-            }
-          }
-        )
+          })
         .then(function(response) {
           if (response.data.isSuccess) {
-            //            console.log('操作人员列表',response.data.objects);
-            let operatorList = [];
-            response.data.objects.forEach(function(item, index, arr) {
-              const operator = {
-                value: item.name,
-                id: item.id
-              };
-              operatorList.push(operator);
-            });
-            that.operatorList = operatorList;
+            const kepRes = response.data.objects
+            that.userCodeList = kepRes
           } else {
             that.$message.success("加载数据失败~");
           }
