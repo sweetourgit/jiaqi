e6f723574981388f660b35c0cbf0230b95a1d157
zhangyi643 <18341314421@163.com>
2019-10-25 17:34:00 +0800

    周边借款模块

---
+++ src/page/Finance/aroundBorrowingManagement/apply/applyFornoIncome.vue
@@ -0,0 +1,472 @@
+<template>
+  <div class="vivo" style="position:relative" id="applyFor">
+    <!--申请预付款-->
+    <el-dialog title="申请" :visible="dialogFormVisible" style="margin:-80px 0 0 0;" custom-class="city_list" :show-close="false" width="70%" @close="closeAdd">
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+        <div class="buttonDv">
+          <el-button class="el-button" type="primary" @click="submitForm('ruleForm')">申 请</el-button>
+          <el-button class="el-button" type="danger" @click="cancalBtn">取 消</el-button>
+        </div>
+        <el-divider content-position="left">noIncome基本信息</el-divider>
+        <div>
+          <el-form-item label="供应商名称：" prop="supplier" label-width="140px">
+            <el-autocomplete class="inputWidth" v-model="ruleForm.supplier" :fetch-suggestions="querySearchD" placeholder="请输入供应商" @select="handleSelectD" @blur="blurHand"></el-autocomplete>
+          </el-form-item>
+          <el-form-item label="借款类型：" prop="type" label-width="140px">
+            <el-select v-model="ruleForm.type" placeholder="请选择" class="inputWidth">
+              <el-option v-for="item in typeList" :key="item.value" :label="item.label" :value="item.value"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="借款金额：" prop="money" label-width="140px">
+            <el-input v-model="ruleForm.money" placeholder="请输入借款金额" class="inputWidth"></el-input>
+          </el-form-item>
+          <el-form-item label="摘要：" prop="abstract" label-width="140px">
+            <el-input v-model="ruleForm.abstract" class="inputWidth" placeholder="请输入摘要信息"></el-input>
+          </el-form-item>
+          <el-form-item label="汇款账户：" prop="payAccount" label-width="140px">
+            <el-input v-model="ruleForm.payNumber" class="inputWidthS" placeholder="汇款账号"></el-input>
+            <el-input v-model="ruleForm.payName" class="inputWidthS" placeholder="开户人姓名"></el-input>
+            <el-input v-model="ruleForm.payAccount" class="inputWidthS" placeholder="开户行"></el-input>
+            <el-button type="primary" @click="chooseFun" style="margin-left: 10px">选择</el-button>
+          </el-form-item>
+          <el-form-item label="附件：" label-width="140px" required>
+            <el-upload ref="upload1" class="upload-demo" :action="UploadUrl()" :headers="headers" :on-success="handleSuccess" :on-error="handleError" :on-remove="handleRemove" :before-remove="beforeRemove" :on-exceed="handleExceed" :file-list="fileList">
+              <el-button size="small" type="primary">点击上传</el-button>
+            </el-upload>
+          </el-form-item>
+        </div>
+      </el-form>
+      <!--选择收款账户-->
+      <el-dialog title="选择账户" :visible="dialogFormVisible1" width=60% @close="close" append-to-body>
+        <div class="table_trip" style="width: 100%;">
+          <el-table ref="singleTable" :data="tableDataZH" border style="width: 100%;margin-bottom: 28px;" :highlight-current-row="true" :header-cell-style="getRowClass">
+            <el-table-column prop="linker" label="汇款户名" align="center">
+            </el-table-column>
+            <el-table-column prop="bankcardNo" label="账号" align="center" width="70%">
+            </el-table-column>
+            <el-table-column prop="bankName" label="开户行" align="center">
+            </el-table-column>
+            <el-table-column prop="name" label="备注" align="center">
+            </el-table-column>
+            <el-table-column prop="option" label="操作" align="center" width="100">
+              <template slot-scope="scope">
+                <el-button @click="chooseBtn(scope.row)" type="danger" size="small" class="table_details">选择</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </div>
+
+        <div class="footer" style="text-align: right;">
+          <el-button class="el-button" type="warning" @click="close">取 消</el-button>
+          <!--<el-button class="el-button" type="primary" @click="">确 认</el-button>-->
+        </div>
+      </el-dialog>
+    </el-dialog>
+  </div>
+</template>
+<script type="text/javascript">
+  export default {
+    name: "newTour",
+    components: {},
+    props: {
+      dialogFormVisible: false
+    },
+    data() {
+      return {
+        readOnly: true,
+        ruleForm: {
+          type: '',
+          payNumber: '',
+          payName: '',
+          payAccount: '',
+          payAccountID: '',
+          money: '',
+          abstract: '',
+          supplier: '',// 选择的供应商
+          supplierName: '',
+          supplierID: '',// 选择的供应商ID
+          number: '',
+          buy_type: ''
+        },
+
+        typeList: [
+          {
+            value: 1,
+            label: '地接'
+          },
+          {
+            value: 2,
+            label: '机票（本公司）'
+          },
+          {
+            value: 3,
+            label: '机票（非本公司）'
+          },
+          {
+            value: 4,
+            label: '小费'
+          },
+          {
+            value: 5,
+            label: '签证'
+          },
+          {
+            value: 6,
+            label: '地接（其他）'
+          },
+          {
+            value: 7,
+            label: '火车票'
+          },
+          {
+            value: 8,
+            label: '汽车票'
+          },
+          {
+            value: 9,
+            label: '船票'
+          },
+          {
+            value: 10,
+            label: '其他'
+          },
+          {
+            value: 11,
+            label: '机票押金'
+          },
+          {
+            value: 12,
+            label: '火车票押金'
+          }
+        ],
+
+        supplierList: [],// 供应商列表
+        rules: {
+          supplier: [{ required: true, message: '供应商不能为空!', trigger: 'change' }],
+          type: [{ required: true, message: '类型不能为空!', trigger: 'change' }],
+          money: [{ required: true, message: '借款金额不能为空!', trigger: 'blur' }],
+          abstract: [{ required: true, message: '摘要不能为空!', trigger: 'blur' }],
+          payAccount: [{ required: true, message: '收款账户不能为空!', trigger: 'change' }]
+        },
+
+        dialogFormVisible1: false,
+        tableDataZH: [],
+
+        fileList: []
+      }
+    },
+    computed: {
+      // 计算属性的 getter
+      headers(){
+        return {
+          'Authorization': 'Bearer ' + localStorage.getItem('token')
+        }
+      }
+    },
+    watch: {
+      dialogFormVisible: {
+        handler: function () {
+          if(this.dialogFormVisible){
+            this.loadSupplier();
+//            alert(this.type);
+          }
+        }
+      }
+    },
+    methods: {
+      // 表格头部背景颜色
+      getRowClass({ row, column, rowIndex, columnIndex }) {
+        if (rowIndex === 0) {
+          return 'background:#F7F7F7;color:rgb(85, 85, 85);'
+        } else {
+          return ''
+        }
+      },
+      closeAdd() {
+        this.ruleForm = {
+          type: '',
+          payNumber: '',
+          payName: '',
+          payAccount: '',
+          payAccountID: '',
+          money: '',
+          abstract: '',
+          supplier: '',
+          supplierID: ''
+        };
+        this.fileList = [];
+        this.$emit('close', false);
+      },
+      cancalBtn(){
+        if(this.ruleForm.collectionTime != '' || this.ruleForm.explain != '' || this.ruleForm.payAccount != '' || this.ruleForm.money != '' || this.ruleForm.abstract != '' || this.fileList.length != 0){
+          this.$confirm("是否取消本次添加?", "提示", {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning"
+          }).then(() => {
+            this.$message.success("已取消添加");
+            this.closeAdd();
+          }).catch(() => {
+
+          });
+        }else{
+          this.closeAdd();
+        }
+      },
+      submitForm(formName) {
+        const that = this;
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+
+            let fileArr = [];
+            console.log(that.fileList);
+            if(that.fileList.length != 0){
+              that.fileList.forEach(function (item, index, arr) {
+                fileArr.push({
+                  name: item.name,
+                  url: item.response.data.url
+                });
+              })
+            }else{
+              fileArr = [];
+            }
+
+            this.ruleForm.supplierID = 6;
+//            const data = {
+//              "supplier_code": this.ruleForm.supplierID,
+//              "supplier_name": this.ruleForm.supplierName,
+//              "periphery_type": 1,
+//              "loan_type": this.ruleForm.type,
+//              "loan_money": this.ruleForm.money,
+//              "mark": this.ruleForm.abstract,
+//              "remittance_account": this.ruleForm.payNumber,
+//              "account_name": this.ruleForm.payName,
+//              "opening_bank": this.ruleForm.payAccount,
+//              "number": this.ruleForm.number,
+//              "file": fileArr,
+//              "create_uid": sessionStorage.getItem('id'),
+//              "org_id": sessionStorage.getItem('orgID'),
+//              "buy_type": this.ruleForm.buy_type
+//            };
+//            console.log(data);
+            this.$http.post(this.GLOBAL.serverSrcPhp + '/api/v1/loan/periphery-loan/add', {
+              "supplier_code": this.ruleForm.supplierID,
+              "supplier_name": this.ruleForm.supplierName,
+              "periphery_type": 1,
+              "loan_type": this.ruleForm.type,
+              "loan_money": this.ruleForm.money,
+              "mark": this.ruleForm.abstract,
+              "remittance_account": this.ruleForm.payNumber,
+              "account_name": this.ruleForm.payName,
+              "opening_bank": this.ruleForm.payAccount,
+              "number": this.ruleForm.number,
+              "file": fileArr,
+              "create_uid": sessionStorage.getItem('id'),
+              "org_id": sessionStorage.getItem('orgID'),
+              "buy_type": this.ruleForm.buy_type
+            }).then(res => {
+              console.log(res);
+              if (res.data.code == 200) {
+                that.$message({
+                  type: 'success',
+                  message: '创建成功!'
+                });
+                that.closeAdd();
+//                that.$emit('loadData');
+              } else {
+                if(res.data.message){
+                  that.$message({
+                    type: 'warning',
+                    message: res.data.message
+                  });
+                }else{
+                  that.$message({
+                    type: 'warning',
+                    message: '创建失败'
+                  });
+                }
+              }
+            }).catch(err => {
+              console.log(err)
+            })
+
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      // 选择账户function
+      chooseFun(){
+        const that = this;
+
+        if(this.ruleForm.supplierID === ''){
+          this.$message.warning("请先选择供应商~");
+        }else{
+          this.dialogFormVisible1 = true;
+          this.ruleForm.supplierID = 6;//暂时替代，获取全部的接口没出
+          this.$http.post(this.GLOBAL.serverSrc + "/universal/supplier/api/supplierget", {
+            "id": this.ruleForm.supplierID
+          },).then(function (obj) {
+            console.log(obj);
+            if(obj.data.isSuccess){
+              that.tableDataZH = obj.data.objects;
+            }else{
+              that.$message.warning("加载账户信息失败");
+            }
+          }).catch(function (obj) {
+            console.log(obj)
+          })
+        }
+
+      },
+      close(){
+        this.dialogFormVisible1 = false;
+      },
+      chooseBtn(row){
+        this.ruleForm.payNumber = row.bankcardNo;
+        this.ruleForm.payName = row.linker;
+        this.ruleForm.payAccount = row.bankName;
+        this.ruleForm.payAccountID = row.id;
+        this.close();
+      },
+      // 上传凭证function
+      UploadUrl(){
+        return this.GLOBAL.serverSrcPhp + '/api/v1/upload/pzfiles';
+      },
+      handleSuccess(response, file, fileList){
+//        console.log(file);
+//        console.log(fileList);
+//        console.log('response',response);
+        if(response.code == 200){
+          this.fileList = fileList;
+        }else{
+          if(response.message){
+            this.$message.warning(response.message);
+          }else{
+            this.$message.warning('文件上传失败');
+          }
+//          this.fileList = fileList;
+//          console.log(fileList);
+//          this.fileList = fileList.splice(-1, 1);
+//          for(let i = 0; i < fileList.length; i++){
+//            console.log(i);
+//          }
+
+//          console.log(this.fileList);
+//          this.fileList = {};
+//          this.$refs.upload1.clearFiles();
+        }
+      },
+      handleError(err, file, fileList){
+        this.$message.warning(`文件上传失败，请重新上传！`);
+      },
+      handleRemove(file, fileList) {
+        console.log(file, fileList);
+        this.fileList = fileList;
+      },
+      handleExceed(files, fileList) {
+        this.$message.warning(`平台订单只支持一个附件上传！`);
+      },
+      beforeRemove(file, fileList) {
+        return this.$confirm(`确定移除 ${ file.name }？`);
+      },
+
+      // 供应商选择
+      querySearchD(queryString, cb){
+        const supplierList = this.supplierList;
+        var results = queryString ? supplierList.filter(this.createFilter1(queryString)) : supplierList;
+        // 调用 callback 返回建议列表的数据
+        cb(results);
+      },
+      createFilter1(queryString) {
+        return (supplierList) => {
+          return (supplierList.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+        };
+      },
+      handleSelectD(item){
+        console.log(item);
+        this.ruleForm.supplierID = item.id;
+
+      },
+      blurHand(){
+        const that = this;
+        let ida = '';
+        if(that.ruleForm.supplier == ''){
+          that.ruleForm.supplierID = '';
+        }else{
+          this.supplierList.forEach(function (item, index, arr) {
+            if(that.ruleForm.supplier == item.value){
+              ida = item.id;
+            }
+          });
+          if(ida){
+            that.ruleForm.supplierID = ida;
+          }else{
+            that.ruleForm.dsupplierID = '';
+          }
+        }
+      },
+
+      // 加载供应商信息
+      loadSupplier(){
+        const that = this;
+        this.$http.post(this.GLOBAL.serverSrc + "/universal/localcomp/api/list", {
+          object: {
+            "name": '',
+            "isDeleted": 0
+          }
+        }).then(function(obj) {
+          console.log('获取供应商',obj);
+          if(obj.data.isSuccess){
+            let supplierObj = [];
+            obj.data.objects.forEach(function (item, index, arr) {
+              const supplier = {
+                'value' : item.name,
+                'id' : item.id
+              };
+              supplierObj.push(supplier);
+            });
+            that.supplierList = supplierObj;
+          }
+        }).catch(function(obj) {
+          console.log(obj);
+        });
+      }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    }
+  }
+
+</script>
+<style lang="scss">
+  #applyFor .buttonDv {
+    position: absolute;
+    top: 8px;
+    right: 3%;
+  }
+  #applyFor .inputWidth {
+    min-width: 400px;
+    width: 60%;
+  }
+  #applyFor .inputWidthHalf{
+    min-width: 200px;
+    width: 28.5%;
+    margin-right: 2%;
+  }
+  .inputWidthS{
+    width: 19%;
+    margin-right: 10px;
+  }
+  #applyFor .el-upload-list__item{
+    margin-top: 10px;
+  }
+  #applyFor .el-divider__text, #applyFor .el-link{
+    font-size: 16px;
+  }
+
+</style>
--- src/page/Finance/aroundBorrowingManagement/aroundBorrowingManagement.vue
+++ src/page/Finance/aroundBorrowingManagement/aroundBorrowingManagement.vue
@@ -4,7 +4,7 @@
       <el-tab-pane label="无收入借款" name="noIncomeBorrowing"></el-tab-pane>
       <el-tab-pane label="预付款管理" name="advancePaymentAround"></el-tab-pane>
       <el-tab-pane label="余额支付借款" name="balancePaymentBorrowing"></el-tab-pane>
-      <el-tab-pane label="需要您审批" name="pendingApproval"></el-tab-pane>
+      <el-tab-pane :label="'需要您审批（'+ (noIncomeNum + advanceNum + balanceNum) +'）'" name="pendingApproval"></el-tab-pane>
     </el-tabs>
     <router-view></router-view>
   </div>
@@ -15,6 +15,9 @@
       return {
         activeName: 'noIncomeBorrowing',
         url_is: true,
+        noIncomeNum: 0,
+        advanceNum: 0,
+        balanceNum: 0
       };
     },
     methods: {
@@ -28,6 +31,34 @@
         } else if (tab.name == 'pendingApproval') {
           this.$router.push({ path: "/aroundBorrowingManagement/pendingApproval" });
         }
+      },
+      loadData(periphery_type){
+        const that = this;
+        this.$http.post(this.GLOBAL.serverSrcPhp + "/api/v1/loan/periphery-loan/examinepage", {
+          "pageIndex": this.currentPage,
+          "pageSize": this.pageSize,
+          "create_uid": '',
+          "supplier_code": '',
+          "start_at": '',
+          "end_at": '',
+          "periphery_type": periphery_type,
+          "approval_status": '',
+          "id": ""
+        }, ).then(function(response) {
+          if (response.data.code == '200') {
+            if(periphery_type == 1){
+              that.noIncomeNum = response.data.data.list.length;
+            }else if(periphery_type == 2){
+              that.advanceNum = response.data.data.list.length;
+            }else if(periphery_type == 3){
+              that.balanceNum = response.data.data.list.length;
+            }
+          } else {
+            that.$message.success("加载数据失败~");
+          }
+        }).catch(function(error) {
+          console.log(error);
+        });
       }
     },
     watch: {
@@ -35,11 +66,11 @@
         handler: function(val, oldVal){
           if (val.path == "/aroundBorrowingManagement/noIncomeBorrowing"){
             this.activeName = "noIncomeBorrowing"
-          }else if(val.path == "/externalOrderList/advancePaymentAround"){
+          }else if(val.path == "/aroundBorrowingManagement/advancePaymentAround"){
             this.activeName = "advancePaymentAround"
-          }else if(val.path == "/externalOrderList/balancePaymentBorrowing"){
+          }else if(val.path == "/aroundBorrowingManagement/balancePaymentBorrowing"){
             this.activeName = "balancePaymentBorrowing"
-          }else if(val.path == "/externalOrderList/pendingApproval"){
+          }else if(val.path == "/aroundBorrowingManagement/pendingApproval"){
             this.activeName = "pendingApproval"
           }
         },
@@ -50,7 +81,19 @@
     created() {
       if (this.$route.path == "/aroundBorrowingManagement/noIncomeBorrowing"){
         this.activeName = "noIncomeBorrowing"
+      }else if(this.$route.path == "/aroundBorrowingManagement/advancePaymentAround"){
+        this.activeName = "advancePaymentAround"
+      }else if(this.$route.path == "/aroundBorrowingManagement/balancePaymentBorrowing"){
+        this.activeName = "balancePaymentBorrowing"
+      }else if(this.$route.path == "/aroundBorrowingManagement/pendingApproval"){
+        this.activeName = "pendingApproval"
       }
+
+      // 加载待审批数量
+      this.loadData(1);
+      this.loadData(2);
+      this.loadData(3);
+
     },
   };
 
