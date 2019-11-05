6370810f844c9718b56a17b0ba08fc34894f3334
zhangyi643 <18341314421@163.com>
2019-11-05 15:32:30 +0800

    外部订单，noCost文件删除

--- src/router/index.js
+++ src/router/index.js
@@ -40,9 +40,7 @@ import orderList from '@/page/orderManagement/orderList/orderList'
 import externalOrderList from '@/page/orderManagement/externalOrderList/externalOrderList'
 import canRecognition from '@/page/orderManagement/externalOrderList/canRecognition'
 import noRecognition from '@/page/orderManagement/externalOrderList/noRecognition'
-import noCost from '@/page/orderManagement/externalOrderList/noCost'
 import recognitionMsg from '@/page/orderManagement/externalOrderList/recognitionMsg/recognitionMsg'
-// import recognitionDo from '@/page/orderManagement/externalOrderList/recognitionMsg/recognitionDo'
 import importHistory from '@/page/orderManagement/externalOrderList/importHistory'
 import loanManagement from '@/page/Finance/loanManagement/loanManagement'
 import advancePayment from '@/page/Finance/advancePayment/advancePayment'
@@ -411,14 +409,6 @@ export default new Router({
           path: 'noRecognition',
           name: '外部订单  /未认收款',
           component: noRecognition,
-          meta: {
-            keepAlive: true,
-            auth: true
-          },
-        }, {
-          path: 'noCost',
-          name: '外部订单  /无成本',
-          component: noCost,
           meta: {
             keepAlive: true,
             auth: true
