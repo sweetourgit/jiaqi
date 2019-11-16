945f7d66954e43e2406f5fea9d6a04aa5ffa447b
haichengxiaodoubao <1711460132@qq.com>
2019-11-16 11:38:40 +0800

    账户验证手机号还是邮箱重复本地验证

--- src/page/contentInfo/merchantInfo/comps/chooseAccount.vue
+++ src/page/contentInfo/merchantInfo/comps/chooseAccount.vue
@@ -15,68 +15,86 @@
 <script>
 export default {
   name: "chooseAccount",
-  data () {
-      return {
-          data: [{
+  data() {
+    return {
+      data: [
+        {
           id: 1,
-          label: '一级 1',
-          children: [{
-            id: 4,
-            label: '二级 1-1',
-            children: [{
-              id: 9,
-              label: '三级 1-1-1'
-            }, {
-              id: 10,
-              label: '三级 1-1-2'
-            }]
-          }]
-        }, {
+          label: "一级 1",
+          children: [
+            {
+              id: 4,
+              label: "二级 1-1",
+              children: [
+                {
+                  id: 9,
+                  label: "三级 1-1-1"
+                },
+                {
+                  id: 10,
+                  label: "三级 1-1-2"
+                }
+              ]
+            }
+          ]
+        },
+        {
           id: 2,
-          label: '一级 2',
-          children: [{
-            id: 5,
-            label: '二级 2-1'
-          }, {
-            id: 6,
-            label: '二级 2-2'
-          }]
-        }, {
+          label: "一级 2",
+          children: [
+            {
+              id: 5,
+              label: "二级 2-1"
+            },
+            {
+              id: 6,
+              label: "二级 2-2"
+            }
+          ]
+        },
+        {
           id: 3,
-          label: '一级 3',
-          children: [{
-            id: 7,
-            label: '二级 3-1'
-          }, {
-            id: 8,
-            label: '二级 3-2'
-          }]
-        }],
-        defaultProps: {
-          children: 'children',
-          label: 'label'
+          label: "一级 3",
+          children: [
+            {
+              id: 7,
+              label: "二级 3-1"
+            },
+            {
+              id: 8,
+              label: "二级 3-2"
+            }
+          ]
         }
-      };
+      ],
+      defaultProps: {
+        children: "children",
+        label: "label"
+      }
+    };
   },
   methods: {
-      getCheckedNodes(one,two) {
-          console.log(one,two,1111)
-        console.log(this.$refs.tree.getCheckedNodes());
-      },
-      setCheckedNodes(nodes) {
-          console.log(nodes,"nodes")
-        this.$refs.tree.setCheckedNodes([{
+    getCheckedNodes(one, two) {
+      console.log(one, two, 1111);
+      console.log(this.$refs.tree.getCheckedNodes());
+    },
+    setCheckedNodes(nodes) {
+      console.log(nodes, "nodes");
+      this.$refs.tree.setCheckedNodes([
+        {
           id: 5,
-          label: '二级 2-1'
-        }, {
+          label: "二级 2-1"
+        },
+        {
           id: 9,
-          label: '三级 1-1-1'
-        }]);
-      },
+          label: "三级 1-1-1"
+        }
+      ]);
+    },
     //   清空
-      resetChecked() {
-        this.$refs.tree.setCheckedKeys([]);
-      }
+    resetChecked() {
+      this.$refs.tree.setCheckedKeys([]);
+    }
   }
 };
 </script>
