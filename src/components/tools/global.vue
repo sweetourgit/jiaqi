2ce26b628d226e35f1ac957893913f054b52c244
331450663 <331450663@qq.com>
2019-11-18 11:14:38 +0800

    global

--- src/components/tools/global.vue
+++ src/components/tools/global.vue
@@ -1,12 +1,20 @@
 <script>
-  const serverSrc="http://192.168.1.186:3017";
-  const imgUrl="http://192.168.1.186:3009";
-  const jqUrl="http://192.168.1.198:6019";
-  const serverSrcPhp="http://www.around.com";
+  const serverSrc="http://test.dayuntong.com";
+  // const imgUrl="http://test.dayuntong.com";
+  const imgUrl ="https://jiaqi-server.obs.myhwclouds.com";//图片预览地址
+  //http://test.dayuntong.com/upload/obs/api/picture  图片上传地址
+  //const imgUrl="http://192.168.2.65:3024";
+  //const imgUrl="http://192.168.2.65:3009";
+  //const jqUrl="http://192.168.1.198:6019";
+  // const jqUrl="http://192.168.2.6:6019";
+  // const jqUrl="http://49.4.10.11:3019"
+  // const jqUrl="117.78.20.162:3019"
+  const jqUrl="http://test.dayuntong.com/h3wf";
+//  const serverSrcPhp="http://www.around.com";
+  const serverSrcPhp="http://zb.dayuntong.com";
   const token="";
   const userId="";
   const hasEnter=false;
-
   export default {
   serverSrc,//服务器地址
   imgUrl,//图片服务器地址
