d3e64aa86e63d39b8c1d299d174a615fa3833332
zhangyi643 <18341314421@163.com>
2019-11-26 14:30:21 +0800

    业务待认款--显示更改

--- src/page/Finance/businessRecognitionManagement/recognitionDetail.vue
+++ src/page/Finance/businessRecognitionManagement/recognitionDetail.vue
@@ -4,7 +4,7 @@
       <div class="buttonDv">
         <el-button type="primary" @click="closeAdd" style="margin-right: 10px" plain>取消</el-button>
         <el-button type="primary" @click="importOrder" v-if="baseInfo.rece_type == '2' && baseInfo.rec_mode == '订单收款' && baseInfo.can_match == '2'">导入订单明细</el-button>
-        <el-button type="primary" @click="backoutBtn" v-if="baseInfo.status_rece != '12'">撤销</el-button>
+        <el-button type="primary" @click="backoutBtn" v-if="baseInfo.status_rece != '12' && baseInfo.rec_mode!='票付通余额支付'">撤销</el-button>
       </div>
       <!--<p class="stepTitle">基本信息</p>-->
       <el-divider content-position="left">基本信息</el-divider>
