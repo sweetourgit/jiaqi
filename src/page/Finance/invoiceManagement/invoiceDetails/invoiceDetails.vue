d8c20b21c43ea7b0e8d86428152494af1c4b28f1
liuying2018 <942792546@qq.com>
2019-12-06 09:44:46 +0800

    更新发票管理

---
+++ src/page/Finance/invoiceManagement/invoiceDetails/invoiceDetails.vue
@@ -0,0 +1,362 @@
+<template>
+  <div>
+    <!--详情-->
+    <el-dialog title="详情" :visible.sync="dialogFormOrder" custom-class="city_list dialogOrder" style="margin-top:-100px" width="1200px"
+      @close="cancelInfoOrder()">
+      <div class="controlButton">
+        <el-button class="ml13" @click="cancelInfoOrder()">取 消</el-button>
+        <el-button class="ml13" v-if="invoiceList.state!='3'" type="primary" @click="rejectedIncoice(invoiceID)">驳回</el-button>
+        <el-button type="primary" v-if="invoiceList.state=='1'" @click="openInvoice(invoiceID)" class="ml13">开票</el-button>
+        <el-button type="primary" v-if="invoiceList.state=='2'" class="ml13">换发票</el-button>
+      </div>
+      <div class="planBorder">
+        <div class="order-title"><span>基本信息</span></div>
+        <div>
+          <div v-if="invoiceList.state=='1'" class="state01">待开票</div>
+          <div v-if="invoiceList.state=='2'">
+            <div class="state02">已开票</div>
+            <div class="state03">发票号码:{{invoiceList.invoiceNumber}}</div>
+          </div>
+          <div v-if="invoiceList.state=='3'" class="state04">开票驳回</div>
+        </div>
+        <div class="pro-info">
+          <table width="100%">
+            <tr>
+              <td width="33%">
+                <div width="80" class="fl">ID:</div>
+                <div class="fl ml13">{{invoiceList.id}}</div>
+              </td>
+              <td width="33%">
+                <div width="80" class="fl">申请人:</div>
+                <div class="fl ml13">{{invoiceList.userName}}</div>
+              </td>
+              <td width="33%">
+                <div width="80" class="fl">申请时间:</div>
+                <div class="fl ml13">{{invoiceList.createTime | formatDate}}</div>
+              </td>
+            </tr>
+            <tr>
+              <td width="33%">
+                <div width="80" class="fl">发票抬头:</div>
+                <div class="fl ml13">{{invoiceList.invoiceHeader}}</div>
+              </td>
+              <td width="33%">
+                <div width="80" class="fl">纳税人识别号:</div>
+                <div class="fl ml13">{{invoiceList.taxpayerIDNumber}}</div>
+              </td>
+              <td width="33%">
+                <div width="80" class="fl">手机号:</div>
+                <div class="fl ml13">{{invoiceList.tel}}</div>
+              </td>
+            </tr>
+            <tr>
+              <td width="33%">
+                <div width="80" class="fl">发票项目:</div>
+                <div class="fl ml13">
+                  <div v-if="invoiceList.invoiceItem=='1'">旅游费</div>
+                </div>
+              </td>
+              <td width="33%">
+                <div width="80" class="fl">发票金额:</div>
+                <div class="fl ml13">{{invoiceList.invoicePrice}}</div>
+              </td>
+              <td width="33%">
+                <div width="80" class="fl">直客/商户:</div>
+                <div class="fl ml13">
+                  <div v-if="invoiceList.collectionType=='1'">商户</div>
+                  <div v-if="invoiceList.collectionType=='2'">直客</div>
+                </div>
+              </td>
+            <tr>
+              <td width="33%">
+                <div width="80" class="fl">账号:</div>
+                <div class="fl ml13">{{invoiceList.cardNumber}}</div>
+              </td>
+              <td width="33%">
+                <div width="80" class="fl">开户行:</div>
+                <div class="fl ml13">{{invoiceList.bankName}}</div>
+              </td>
+              <td width="33%">
+                <div width="80" class="fl">地址:</div>
+                <div class="fl ml13">{{invoiceList.address}}</div>
+              </td>
+            </tr>
+            <tr>
+              <td width="33%">
+                <div width="80" class="fl">发票类型:</div>
+                <div class="fl ml13">
+                  <div v-if="invoiceList.invoiceID=='1'">纸质发票</div>
+                </div>
+              </td>
+              <td width="33%">
+                <div width="80" class="fl">个人/单位:</div>
+                <div class="fl ml13">
+                  <div v-if="invoiceList.invoiceType=='1'">个人</div>
+                  <div v-if="invoiceList.invoiceType=='2'">单位</div>
+                </div>
+              </td>
+              <td width="33%">
+                <div width="80" class="fl">发票时间:</div>
+                <div class="fl ml13" v-if="invoiceList.selStartGrantTime !='0' ">{{invoiceList.selStartGrantTime}}</div>
+              </td>
+            </tr>
+            <tr>
+              <td width="33%">
+                <div width="80" class="fl">收款单号:</div>
+                <div class="fl ml13">{{invoiceList.collectionID}}</div>
+              </td>
+              <td width="33%">
+                <div width="80" class="fl">报账团号:</div>
+                <div class="fl ml13">{{invoiceList.accountGroupCode}}</div>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div class="order-title"><span>关联订单</span></div>
+        <el-table :data="tableDate" ref="multipleTable" class="table" :header-cell-style="getRowClass" border :cell-style="getCellClass">
+          <el-table-column prop="orderCode" label="订单编号" align="center"></el-table-column>
+          <el-table-column prop="title" label="产品名称" align="center"></el-table-column>
+          <el-table-column prop="groupCode" label="团期计划" align="center"></el-table-column>
+          <el-table-column prop="orderCreateTime" :formatter='dateFormat' label="下单日期" align="center"></el-table-column>
+          <el-table-column prop="kpPrice" label="已付金额" align="center"></el-table-column>
+          <el-table-column prop="skPrice" label="剩余开票金额" align="center"></el-table-column>
+        </el-table>
+      </div>
+    </el-dialog>
+    <!--开票、换票弹窗-->
+    <el-dialog :title="title" :visible.sync="openInvoiceShow" custom-class="city_list dialogOrder" style="margin-top:-100px" width="1200px"
+      @close="closeOpenInvoice()">
+      <el-form :model="conForm" :rules="rules" ref="conForm">
+        <div class="controlButton">
+          <el-button class="ml13" @click="closeOpenInvoice()">取 消</el-button>
+          <el-button type="primary" @click="openInvoicement(invoiceID)" class="ml13">开票</el-button>
+        </div>
+        <el-form :model="ruleFormSeach" ref="ruleFormSeach" label-width="120px">
+          <el-form-item label="单张发票金额:" prop="invoicePrice">
+            <el-input v-model="ruleFormSeach.invoicePrice" class="w200" placeholder="请输入发票金额"></el-input>
+            <el-button type="primary">拆分发票</el-button>
+          </el-form-item>
+        </el-form>
+        <div class="associated">
+          <div class="warning"><i class="el-icon-warning"></i></div>
+          <div class="fl">已关联<span class="relateditems">{{invoiceDate.length}}</span>项</div>
+          <div class="aggregate">总计:<span>{{invoiceList.invoicePrice | numFilter}}</span>元</div>
+        </div>
+        <el-table :data="invoiceDate" ref="multipleTable" class="table" :header-cell-style="getRowClass" border :cell-style="getCellClass">
+          <el-table-column prop="invoiceHeader" label="发票抬头" align="center"></el-table-column>
+          <el-table-column prop="taxpayerIDNumber" label="纳税人识别号" align="center"></el-table-column>
+          <el-table-column prop="tel" label="电话" align="center"></el-table-column>
+          <el-table-column prop="cardNumber" label="账号" align="center"></el-table-column>
+          <el-table-column prop="address" label="地址" align="center"></el-table-column>
+          <el-table-column prop="bankName" label="开户行" align="center"></el-table-column>
+          <el-table-column label="发票金额" align="center" min-width="120">
+            <template slot-scope="scope">
+              <el-input v-model="invoiceAmount" class="w150"></el-input>
+              <div class="validation" v-if="invoiceAmount == '' && a == true">发票金额不能为空</div>
+            </template>
+          </el-table-column>
+          <el-table-column label="发票号码" align="center" min-width="120">
+            <template slot-scope="scope">
+              <el-input v-model="invoiceNumber" class="w150"></el-input>
+              <div class="validation" v-if="invoiceNumber == '' && a == true">发票号码不能为空</div>
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="order-title"><span>关联订单</span></div>
+        <el-table :data="tableDate" ref="multipleTable" class="table" :header-cell-style="getRowClass" border :cell-style="getCellClass">
+          <el-table-column prop="orderCode" label="订单编号" align="center"></el-table-column>
+          <el-table-column prop="title" label="产品名称" align="center"></el-table-column>
+          <el-table-column prop="groupCode" label="团期计划" align="center"></el-table-column>
+          <el-table-column prop="orderCreateTime" :formatter='dateFormat' label="下单日期" align="center"></el-table-column>
+          <el-table-column prop="kpPrice" label="已付金额" align="center"></el-table-column>
+          <el-table-column prop="skPrice" label="剩余开票金额" align="center"></el-table-column>
+        </el-table>
+      </el-form>
+    </el-dialog>
+  </div> 
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  props: {
+    invoiceID:0,
+    variable: 0,
+    dialogType: 0
+  },
+  data() {
+    return {
+      dialogFormOrder:false,// 详情弹窗
+      invoiceList:{},// 基本信息数组
+      tableDate:[],// 详情弹窗中关联订单表格
+      title:"",//换票、开票标题
+      openInvoiceShow:false,
+      ruleFormSeach:{// 单张发票金额
+        invoicePrice:'',
+      },
+      invoiceDate:[],//发票表格
+      invoiceAmount:'',// 输入发票金额
+      invoiceNumber:'',// 输入发票号
+      a:false,
+      conForm:{},
+      rules:{},
+    };
+
+  },
+  filters: {
+    numFilter (value) {
+      // 截取当前数据到小数点后两位
+      let realVal = parseFloat(value).toFixed(2)
+      return realVal
+    },
+    formatDate: function (value) {//截取详情时间格式
+      return moment(value).format('YYYY-MM-DD')
+    }
+  },
+  mounted () {
+  },
+  watch: {
+    variable: function() {
+      if (this.dialogType == 1) { // 详情
+        setTimeout(() => {
+          this.getInvoice(this.invoiceID);
+        }, 200);
+        this.dialogFormOrder = true;
+      }else if(this.dialogType == 2){
+        setTimeout(() => {
+          this.rejectedIncoice(this.invoiceID);
+        }, 200);
+      }else if(this.dialogType == 3){
+        setTimeout(() => {
+          this.getInvoice(this.invoiceID);
+          this.openInvoiceShow = true;
+          this.title = "开票"
+        }, 200);
+      }
+    },
+  },
+  methods: {
+    // 起始时间格式转换
+    dateFormat: function(row, column) {
+      let date = row[column.property];
+      if(date == undefined) {
+        return '';
+      }
+      return moment(date).format('YYYY-MM-DD')
+    }, 
+
+    getRowClass({ row, column, rowIndex, columnIndex }) {//表格头部颜色
+      if (rowIndex == 0) {
+        return "background:#f7f7f7;height:60px;textAlign:center;color:#333;fontSize:15px";
+      } else {
+        return "";
+      }
+    },
+    getCellClass() {
+      return "textAlign:center";
+    },
+    cancelInfoOrder(){ // 关闭详情弹窗
+      this.dialogFormOrder = false;
+      this.tableDate = [];
+    },
+    getInvoice(ID){//详情弹窗
+      this.$http.post(this.GLOBAL.serverSrc + "/finance/Receipt/api/get", {
+        id: ID
+      }).then(res => {
+        if (res.data.isSuccess == true) {
+          console.log(res.data.object)
+          this.invoiceList = res.data.object;
+          this.tableDate = res.data.object.ordelist;
+          this.invoiceDate.push(res.data.object);
+        }
+      });
+    },
+    rejectedIncoice(ID){// 驳回
+      console.log(ID)
+      this.$confirm("是否驳回本次发票信息?", "提示", {
+         confirmButtonText: "确定",
+         cancelButtonText: "取消",
+         type: "warning"
+      })
+      .then(res => {
+        this.$http.post(this.GLOBAL.serverSrc + '/finance/Receipt/api/regression',{
+          "object": {
+            "id": ID,
+          },
+        }).then(res => {
+          if(res.data.isSuccess == true){
+             this.$message.success("驳回成功");
+             this.$parent.pageList();
+             this.dialogFormOrder = false;
+            }
+         })
+      })
+      .catch(() => {
+        this.$message({
+          type: "info",
+          message: "已取消"
+        });
+      });
+    },
+    closeOpenInvoice(){// 关闭开票、换票弹窗
+      this.openInvoiceShow = false;
+      this.invoiceDate = [];
+    },
+    openInvoice(ID){ // 在详情弹窗里点击开票按钮
+      this.invoiceDate = [];
+      this.getInvoice(ID);
+      this.dialogFormOrder = false;
+      this.openInvoiceShow = true;
+    },
+    invoiceOnly(ID){ // 验证发票号唯一性
+      this.$http.post(this.GLOBAL.serverSrc + "/finance/Receipt/api/exisinvoicenumber", {
+        id: ID,
+        invoiceNumber:this.invoiceNumber
+      }).then(res => {
+        if (res.data.isSuccess == false && this.invoiceNumber !== '') {
+          this.$confirm("该发票号码已存在?", "提示", {
+           confirmButtonText: "确定",
+           cancelButtonText: "取消",
+           type: "warning"
+          })
+        }
+      });
+    },
+    openInvoicement(ID){ // 点击开票按钮
+      this.invoiceOnly(); // 验证发票号唯一方法
+      this.a = true;
+      let guest = JSON.parse(JSON.stringify(this.conForm));
+      console.log(guest)
+      // this.$http.post(this.GLOBAL.serverSrc + "/finance/Receipt/api/confirmr", {
+      //   "object":{
+      //     receiptList:0,
+      //     receiptID:ID
+      //   }
+      // })
+    },
+  }
+};
+</script>
+
+<style scoped>
+/*下单弹窗团期信息样式*/
+.planBorder{width: 95%;margin: 0 30px 10px 0;font-size: 14px;line-height: 25px;}
+.order-title{font-size: 14pt; color:#000;line-height: 40px;}
+.controlButton{position: absolute; top: 8px; right: 10px;}
+.fl{float: left;}
+.ml13{margin: 0 0 0 13px;}
+.state01{text-align: center; background: #ffaa00; border-radius: 5px; color: #fff;width:60px;padding: 2px; margin: 10px 0 0 0;}
+.state02{text-align: center; background: #008000; border-radius: 5px; color: #fff;width:60px;padding: 2px; margin: 10px 0 0 0;float: left;}
+.state03{color:#008000;float: left; line-height: 30px; margin: 10px 0 0 10px; font-weight: bold;}
+.state04{text-align: center; background: #eb0000; border-radius: 5px; color: #fff;width:60px;padding: 2px; margin: 10px 0 0 0;}
+.pro-info{margin: 15px 0 20px 0;line-height: 30px;}
+.table{margin: 10px 0 0 0;}
+.w200{width: 200px;}
+.associated{background: #e6f3fc;border: 1px solid #d5f0fc; line-height: 30px;border-radius: 5px;overflow: hidden;}
+.warning{float: left;color: #108ee9;margin: 0 0 0 15px;font-size: 16px;}
+.relateditems{color: #108ee9; margin: 0 5px 0 5px;}
+.aggregate{margin: 0 0 0 15px;float: left;}
+.aggregate span{ margin: 0 0 0 15px;}
+.w150{width: 150px;}
+.validation{color: red;width: 140px;line-height: 30px;}
+</style>
