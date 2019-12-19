27e416a566e96f4c31ac15f014349bef9432b382
jenny7272 <jenny_0013@163.com>
2019-12-19 10:15:19 +0800

    更新报销管理12/19-1.0

---
+++ src/page/Finance/balanceOrder/commercia/commercia.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="main">
+      <div>我是商户欠款</div>
+        <!-- <teamOrder-insert :planId="planId" :variable="variable" :dialogType="dialogType"></teamOrder-insert> -->
+    </div>
+    <!-- </div> -->
+  </div>
+</template>
+
+<script>
+import commercia from "./commercia";
+export default {
+  name: "commercia",
+  components: {
+    commercia
+    // "teamOrder-insert":teamOrderInsert,
+  },
+  data() {
+    return {
+      
+    };
+  },
+  created() {
+   
+  },
+  mounted () {
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped>
+ 
+</style>
