447795bdb844888e4b9270594fb58e878f4b6b18
Gao Yun <67802052@qq.com>
2019-12-20 17:02:09 +0800

    收款同业，直客打印。商户联想项自适应。发票校验。凭证不回显。借款详情状态不刷新。

--- src/components/tools/global.vue
+++ src/components/tools/global.vue
@@ -1,10 +1,10 @@
 <script>
   // 非周边 - 正式
-  // const serverSrc="http://api.dayuntong.com"
-  // const jqUrl = 'http://49.4.28.5:3019/h3wf'
+  const serverSrc="http://api.dayuntong.com"
+  const jqUrl = 'http://49.4.28.5:3019/h3wf'
   // 测试
-  const serverSrc="http://192.168.2.81:3017";
-  const jqUrl = 'http://192.168.2.81:3019/h3wf'
+  // const serverSrc="http://192.168.2.81:3017";
+  // const jqUrl = 'http://192.168.2.81:3019/h3wf'
 
   // const serverSrc="/api";
   // const jqUrl = '/api/h3bpm'
