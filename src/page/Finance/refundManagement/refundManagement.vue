f4dbec062d0bb0b2b46b4a97d48276900b9ec114
liuying2018 <942792546@qq.com>
2019-12-25 17:02:52 +0800

    更新路由文件

---
+++ src/page/Finance/refundManagement/refundManagement.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  退款管理
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+
