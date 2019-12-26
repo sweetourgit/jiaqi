7be2219b9bdf31fb480c3e6c5a731495378bfe93
foolseward <foolseward@163.com>
2019-12-26 10:33:57 +0800

    d

---
+++ src/page/Finance/checkSheetDetail/subs/team/comps/printGround/subs/incomeBar.vue
@@ -0,0 +1,52 @@
+<style>
+  @import "~@/page/Finance/checkSheetDetail/subs/team/comps/printGround/print.css";
+</style>
+<template>
+  <tr>
+    <td class="base" style="width:5%;">
+      <div class="cell">123</div>
+    </td>
+    <td class="base">
+      <div class="cell"></div>
+    </td>
+    <td class="base">
+      <div class="cell"></div>
+    </td>
+    <td class="base" style="width:5%;">
+      <div class="cell"></div>
+    </td>
+    <td class="base">
+      <div class="cell"></div>
+    </td>
+    <td class="base" style="width:5%;">
+      <div class="cell"></div>
+    </td>
+    <td class="base" style="width:5%;">
+      <div class="cell"></div>
+    </td>
+    <td class="base" style="width:5%;">
+      <div class="cell"></div>
+    </td>
+    <td class="base" style="width:5%;">
+      <div class="cell"></div>
+    </td>
+    <td class="base">
+      <div class="cell"></div>
+    </td>
+    <td class="base">
+      <div class="cell"></div>
+    </td>
+    <td class="base">
+      <div class="cell"></div>
+    </td>
+    <td class="base">
+      <div class="cell"></div>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+
+}
+</script>
