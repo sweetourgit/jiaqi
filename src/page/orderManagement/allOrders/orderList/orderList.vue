6c1d16e2682e14d4e441e7085b507ca5ae22a0c6
haichengxiaodoubao <1711460132@qq.com>
2019-12-26 15:10:34 +0800

    图片下载

--- src/page/orderManagement/allOrders/orderList/orderList.vue
+++ src/page/orderManagement/allOrders/orderList/orderList.vue
@@ -466,10 +466,39 @@ export default {
   },
   methods: {
     moment,
-    adviceNoteDown() {
-      let url = this.fileList[0].url;
-      window.open(url);
+
+    downloadIamge(imgsrc, name) {
+      //下载图片地址和图片名
+      var image = new Image();
+      // 解决跨域 Canvas 污染问题
+      image.setAttribute("crossOrigin", "anonymous");
+      image.onload = function() {
+        var canvas = document.createElement("canvas");
+        canvas.width = image.width;
+        canvas.height = image.height;
+        var context = canvas.getContext("2d");
+        context.drawImage(image, 0, 0, image.width, image.height);
+        var url = canvas.toDataURL("image/png"); //得到图片的base64编码数据
+
+        var a = document.createElement("a"); // 生成一个a元素
+        var event = new MouseEvent("click"); // 创建一个单击事件
+        a.download = name || "photo"; // 设置图片名称
+        a.href = url; // 将生成的URL设置为a.href属性
+        a.dispatchEvent(event); // 触发a的单击事件
+      };
+      image.src = imgsrc;
     },
+    adviceNoteDown() {
+      this.downloadIamge(this.fileList[0].url, this.fileList[0].name);
+      // var alink = document.createElement("a");
+      // alink.href = this.fileList[0].url;
+      // alink.download = this.fileList[0].name; //图片名
+      // alink.click();
+    },
+    // adviceNoteDown() {
+    //   let url = this.fileList[0].url;
+    //   window.open(url);
+    // },
     // 出团通知书的发送
     adviceNoteSend() {},
     handleRemove(file, fileList) {},
