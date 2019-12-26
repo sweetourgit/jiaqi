953e4582f7c9dfe1c2f84135f01a0a061f586c70
foolseward <foolseward@163.com>
2019-12-26 17:51:11 +0800

    d

--- src/page/contentInfo/supplierInfo/supplierDetails/supplierDetails.vue
+++ src/page/contentInfo/supplierInfo/supplierDetails/supplierDetails.vue
@@ -311,6 +311,7 @@ export default {
     downloadPicByUrl(url, name){
       const xhr = new XMLHttpRequest();
       xhr.open('GET', url, true);
+      xhr.setRequestHeader('Sec-Fetch-Mode', 'cors')
       xhr.responseType = 'blob';
       xhr.onload = () => {
         if (xhr.status === 200) {
