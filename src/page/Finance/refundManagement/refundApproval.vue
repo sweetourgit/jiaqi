84d4e08c36ea4fb6a4fd2d1b5ecd31f646becdd1
liuying2018 <942792546@qq.com>
2019-12-26 08:51:54 +0800

    退款管理

---
+++ src/page/Finance/refundManagement/refundApproval.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  需要您审批
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  created() {
+  },
+  methods: {
+     
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+
---
+++ src/page/Finance/refundManagement/refundRecord.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  退款记录
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  created() {
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+
