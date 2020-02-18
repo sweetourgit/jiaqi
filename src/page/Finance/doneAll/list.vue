495709fc9a3604a7ef8eadc0326104f6cb7a3d71
Gao Yun <67802052@qq.com>
2020-02-18 12:05:58 +0800

    已办

--- src/page/Finance/doneAll/list.vue
+++ src/page/Finance/doneAll/list.vue
@@ -2,28 +2,33 @@
 <template>
   <div class="distributor-content">
     <el-tabs v-model="tabShowWhich" @tab-click="handleClick" style="width: 98%;margin: 20px auto;">
-      <el-tab-pane :label="'无收入借款管理(' + approveDataNum +')'" name="reimburse">
-
+      <el-tab-pane :label="'无收入借款管理(' + approveDataNum +')'" name="borrow">
+        <noIn :whereTab="whereTab" @handlePassVal="handleGetAlreadyNum"></noIn>
       </el-tab-pane>
       <el-tab-pane label="预付款管理" name="advance">
-
+        <noIn :whereTab="whereTab"></noIn>
       </el-tab-pane>
-      <el-tab-pane label="报销管理管理" name="borrow"></el-tab-pane>
+      <el-tab-pane label="报销管理" name="reimburse">
+        <reimbursement></reimbursement>
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 
 <script>
   import noIn from './noIn'
+  import reimbursement from './reimbursement'
   export default {
     name: "approveList",
     components: {
-      noIn
+      noIn,
+      reimbursement
     },
     data(){
-      return {
-        approveDataNum:2,
-        tabShowWhich: 'reimburse', // 显示哪一个tab
+      return{
+        whereTab:'borrow',
+        approveDataNum:0,
+        tabShowWhich: 'borrow', // 显示哪一个tab
       }
     },
     created(){
@@ -35,8 +40,13 @@
     methods: {
       // tab切换
       handleClick(tab, event) {
-        // console.log(tab, event);
+        this.whereTab = tab.name
+        // console.log(tab.name);
       },
+      handleGetAlreadyNum(paramsPassCount){
+        console.log(paramsPassCount)
+        this.approveDataNum = paramsPassCount
+      }
     }
   }
 </script>
