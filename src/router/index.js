dc02d6b28eefd04b51f69f6476eeb65f7ae803f3
zhangyi643 <18341314421@163.com>
2020-03-10 16:01:51 +0800

    修改路由文件

--- src/router/index.js
+++ src/router/index.js
@@ -312,7 +312,7 @@ export default new Router({
         }
       ]
     }, {
-      path: '/productLiner/liner',
+      path: '/productLinerDo',
       component: () => import('@/page/productManagement/liner/linerAdd'),
       name: '邮轮/ 添加',
       meta: {
