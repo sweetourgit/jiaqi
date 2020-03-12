863172fe03d35b1943c70a18256075035c5e49f4
foolseward <foolseward@163.com>
2020-03-12 09:53:11 +0800

    d

---
+++ src/page/productManagement/planInventory/liner/price/comps/PriceHeader/PriceHeader.vue
@@ -0,0 +1,32 @@
+<style>
+
+</style>
+
+<template>
+  <Jdatepanel></Jdatepanel>
+</template>
+
+<script>
+/**
+ * @description: 分离业务与日历逻辑
+ */
+import Jdatepanel from './comps/Jdatepanel/Jdatepanel'
+
+export default {
+
+  components: { Jdatepanel },
+
+  data(){
+    return {
+      calendarArr: [],  // 日历面板
+    }
+  },
+
+  methods: {
+    init(){
+
+    }
+  }
+
+}
+</script>
--- src/page/productManagement/planInventory/liner/price/index.vue
+++ src/page/productManagement/planInventory/liner/price/index.vue
@@ -1,13 +1,31 @@
-<style>
-
+<style lang="scss" scoped>
+.page{
+  &>header{
+    display: flex;
+    align-content: center;
+    justify-content: space-between;
+    padding-bottom: 10px;
+    border-bottom: 1px solid #ddd;
+    box-sizing: border-box;
+  }
+}
 </style>
 
 <template>
-  
+  <div class="page">
+    <header>
+      <PriceHeader></PriceHeader>
+      <el-button type="info" size="mini">返回</el-button>
+    </header>
+  </div>
 </template>
 
 <script>
+import PriceHeader from './comps/PriceHeader/PriceHeader.vue'
+
 export default {
+
+  components: { PriceHeader }
 
 }
 </script>
