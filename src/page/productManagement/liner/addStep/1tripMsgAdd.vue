703af7d7e73fa96eb83796abc926e5307249518f
zhangyi643 <18341314421@163.com>
2020-03-19 19:11:40 +0800

    游轮产品

--- src/page/productManagement/liner/addStep/1tripMsgAdd.vue
+++ src/page/productManagement/liner/addStep/1tripMsgAdd.vue
@@ -8,34 +8,17 @@
           <el-button class="el-button" type="danger" @click="cancalBtn">取 消</el-button>
         </div>
         <div>
-          <el-form-item label="舱型：" prop="cabinType" label-width="140px">
-            <!-- <el-input v-model="ruleForm.cabinType" placeholder="请输入" class="inputWidth"></el-input> -->
-            <el-select  v-model="ruleForm.cabinType">
-              <el-option :key="item.id" v-for="item in typeArr" :label="item.name" :value="item.id"></el-option>
-            </el-select>
+          <el-form-item label="标题：" prop="title" label-width="140px">
+            <el-input v-model="ruleForm.title" class="inputWidth" placeholder="请输入" maxlength="30" show-word-limit></el-input>
           </el-form-item>
-          <el-form-item label="名称：" prop="name" label-width="140px">
-            <el-input v-model="ruleForm.name" class="inputWidth" placeholder="请输入"></el-input>
+          <el-form-item label="价钱：" prop="price" label-width="140px">
+            <el-input v-model="ruleForm.price" class="inputWidth" placeholder="请输入"></el-input>
           </el-form-item>
-          <el-form-item label="容纳人数：" prop="person" label-width="140px">
-            <el-input v-model="ruleForm.person" class="inputWidth" placeholder="请输入"></el-input>
-          </el-form-item>
-          <el-form-item label="楼层：" prop="floor" label-width="140px">
-            <el-input v-model="ruleForm.floor" class="inputWidth" placeholder="请输入"></el-input>
-          </el-form-item>
-          <el-form-item label="面积：" prop="area" label-width="140px">
-            <el-input v-model="ruleForm.area" class="inputWidth" placeholder="请输入"></el-input>
-          </el-form-item>
-          <el-form-item label="窗型：" prop="window" label-width="140px">
-            <el-input v-model="ruleForm.window" class="inputWidth" placeholder="请输入"></el-input>
+          <el-form-item label="行程时长：" prop="duration" label-width="140px">
+            <el-input v-model="ruleForm.duration" class="inputWidth" placeholder="请输入"></el-input>
           </el-form-item>
           <el-form-item label="简介：" prop="introduction" label-width="140px">
-            <el-input v-model="ruleForm.introduction" class="inputWidth" placeholder="请输入" type="textarea"></el-input>
-          </el-form-item>
-          <el-form-item label="图片：" label-width="140px">
-            <el-upload ref="upload1" class="upload-demo" :action="UploadUrl1()" :headers="headers" :on-success="handleSuccess1" :on-error="handleError1" :on-remove="handleRemove1" :before-remove="beforeRemove1" :on-exceed="handleExceed1" :file-list="fileList1">
-              <el-button size="small" type="primary">点击上传</el-button>
-            </el-upload>
+            <el-input v-model="ruleForm.introduction" class="inputWidth" placeholder="请输入" type="textarea" maxlength="100" show-word-limit></el-input>
           </el-form-item>
         </div>
       </el-form>
@@ -53,38 +36,31 @@
     data() {
       return {
         ruleForm: {
-          cabinType: '',
-          name: '',
-          person: '',
-          floor: '',
-          area: '',
-          window: '',
+          title: '',
+          price: '',
+          duration: '',
           introduction: ''
         },
         rules: {
-          cabinType: [{ required: true, message: '舱房类型不能为空!', trigger: 'change' }],
-          name: [{ required: true, message: '舱房名称不能为空!', trigger: 'blur' }],
-          person: [{ required: true, message: '舱房容纳人数不能为空!', trigger: 'blur' }]
+          title: [{ required: true, message: '标题不能为空!', trigger: 'blur' }]
         },
-        fileList1: [], // 图片文件
         topTitle: '添加',
-        typeArr: []
       }
     },
     computed: {
-      // 计算属性的 getter
-      headers(){
-        return {
-          'Authorization': 'Bearer ' + localStorage.getItem('token')
-        }
-      }
+      
     },
     watch: {
       dialogFormVisible: {
         handler: function () {
-          if(this.info){
+          if(this.info.type == "edit"){
             this.topTitle = '编辑';
-            this.loadData();
+            this.ruleForm = {
+              title: this.info.tableData[parseInt(this.info.index)].title,
+              price: this.info.tableData[parseInt(this.info.index)].price,
+              duration: this.info.tableData[parseInt(this.info.index)].duration,
+              introduction: this.info.tableData[parseInt(this.info.index)].introduction
+            };
           }else{
             this.topTitle = '添加';
           }
@@ -95,15 +71,11 @@
       // 关闭弹框
       closeAdd() {
         this.ruleForm = {
-          cabinType: '',
-          name: '',
-          person: '',
-          floor: '',
-          area: '',
-          window: '',
+          title: '',
+          price: '',
+          duration: '',
           introduction: ''
         };
-        this.fileList1 = [];
         this.$emit('close', false);
       },
       // 取消按钮事件
@@ -124,62 +96,34 @@
         const that = this;
         this.$refs[formName].validate((valid) => {
           if (valid) {
-            
-            let fileArr = [];
-            if(that.fileList1.length == 0){
-              // that.$message.warning("图片不能为空！");
-            }else{
-              that.fileList1.forEach(function (item, index, arr) {
-                fileArr.push({
-                  pic_id: item.id,
-                  pic_url: item.url
-                });
-              })
+            const num = parseInt(this.info.name) - 1;
+            if(that.info.type == "add"){
+              const item = {
+                title: that.ruleForm.title,
+                price: that.ruleForm.price,
+                duration: that.ruleForm.duration,
+                introduction: that.ruleForm.introduction
+              }
+              // console.log(that.info.tableData);
+              that.info.tableData[parseInt(that.info.index)] = item;
+              // that.info.tableData = that.info.tableData.push(item);
+              // that.$parent.editableTabs[num].content.ruleForm.tableData = that.info.tableData;
+              // console.log(that.$parent.editableTabs[num])
+            }else if(that.info.type == "edit"){
+              const item = {
+                title: that.ruleForm.title,
+                price: that.ruleForm.price,
+                duration: that.ruleForm.duration,
+                introduction: that.ruleForm.introduction
+              }
+              // console.log(parseInt(that.info.index));
+              that.info.tableData = that.info.tableData.splice(parseInt(that.info.index), 1, item);
+              that.info.tableData[parseInt(that.info.index)] = item;
+              // console.log(that.info.tableData)
+              // that.$parent.editableTabs[num].content.ruleForm.tableData[that.info.index] = that.info.tableData;
             }
-            let idStr = '';
-            if(this.info){
-              idStr = this.info;
-            }
-            this.$http.post(this.GLOBAL.serverSrcYL + '/linerapi/v1/liner/liner-cabin/savelinercabin', {
-							"liner_id": localStorage.getItem('liner_id'),
-							"id": idStr,
-							"cabin_type_id": this.ruleForm.cabinType,
-							"name": this.ruleForm.name,
-							"number": this.ruleForm.person,
-							"floor": this.ruleForm.floor,
-							"area": this.ruleForm.area,
-							"window": this.ruleForm.window,
-							"introduce": this.ruleForm.introduction,
-							"pics": fileArr,
-							"create_uid": sessionStorage.getItem('id'),
-							"org_id": sessionStorage.getItem('orgID')
-						}).then(res => {
-							// console.log(res);
-							if (res.data.code == 200) {
-									that.$message({
-									type: 'success',
-									message: '创建成功!'
-									});
-									that.closeAdd();
-
-							} else {
-									if(res.data.message){
-									that.$message({
-											type: 'warning',
-											message: res.data.message
-									});
-									}else{
-									that.$message({
-											type: 'warning',
-											message: '创建失败'
-									});
-									}
-							}
-						}).catch(err => {
-							console.log(err)
-						})
             
-
+            that.closeAdd();
           } else {
             console.log('error submit!!');
             return false;
@@ -187,94 +131,13 @@
         });
       },
 
-      // 上传凭证 function logo
-      UploadUrl1(){
-        return this.GLOBAL.serverSrcPhp + '/api/v1/upload/pzfiles';
-      },
-      handleSuccess1(response, file, fileList){
-        // console.log(response);
-        // console.log(file);
-        if(response.code == 200){
-          this.fileList1.push(response.data);
-        }else{
-          if(response.message){
-            this.$message.warning(response.message);
-          }else{
-            this.$message.warning('文件上传失败');
-          }
-        }
-      },
-      handleError1(err, file, fileList){
-        this.$message.warning(`文件上传失败，请重新上传！`);
-      },
-      handleRemove1(file, fileList) {
-        console.log(file, fileList);
-        this.fileList1 = fileList;
-      },
-      handleExceed1(files, fileList) {
-        this.$message.warning(`平台订单只支持一个附件上传！`);
-      },
-      beforeRemove1(file, fileList) {
-        return this.$confirm(`确定移除 ${ file.name }？`);
-      },
-
       // 加载编辑数据
       loadData(){
         // alert(this.info);
-        const that = this;
-        this.$http.post(this.GLOBAL.serverSrcYL + "/linerapi/v1/liner/liner-cabin/viewlinercabin", {
-          "id": this.info
-        }, ).then(function(response) {
-          console.log('bianji~',response);
-          if (response.data.code == '200') {
-            that.ruleForm = {
-              cabinType: response.data.data.cabin_type_id,
-              name: response.data.data.name,
-              person: response.data.data.number,
-              floor: response.data.data.floor,
-              area: response.data.data.area,
-              window: response.data.data.window,
-              introduction: response.data.data.introduce
-            };
-            that.fileList1 = response.data.data.pics;
-            that.fileList1.forEach(function(item, index, arr){
-              item.id = response.data.data.pics[index].pic_id;
-              item.url = response.data.data.pics[index].pic_url;
-              item.name = response.data.data.pics[index].pic_name;
-            })
-          } else {
-            if(response.data.message){
-              that.$message.warning(response.data.message);
-            }else{
-              that.$message.warning("加载数据失败~");
-            }
-          }
-        }).catch(function(error) {
-          console.log(error);
-        });
       },
-      loadCabin(){
-        // alert(this.info);
-        const that = this;
-        this.$http.post(this.GLOBAL.serverSrcYL + "/linerapi/v1/liner/cabin-type/laststage")
-        .then(function(response) {
-          console.log('moji~',response);
-          if (response.data.code == '200') {
-            that.typeArr = response.data.data.list
-          } else {
-            if(response.data.message){
-              that.$message.warning(response.data.message);
-            }else{
-              that.$message.warning("加载数据失败~");
-            }
-          }
-        }).catch(function(error) {
-          console.log(error);
-        });
-      }
     },
     created() {
-      this.loadCabin();
+      
     },
     mounted() {
 
