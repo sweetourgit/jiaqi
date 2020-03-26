c8c0b70b4c78b575de1147aeb305758f96bd9054
foolseward <foolseward@163.com>
2020-03-26 17:37:47 +0800

    d

--- src/components/tools/global.vue
+++ src/components/tools/global.vue
@@ -32,7 +32,7 @@
   // const jqUrl="http://test.dayuntong.com/h3wf";
   // const serverSrcPhp="http://www.around.com";
   const serverSrcPhp="http://zb.dayuntong.com";
-  const serverSrcYL="http://192.168.1.224";
+  const serverSrcYL="http://web.17lvju.com/app/jialv";
   const token="";
   const userId="";
   const hasEnter=false;
