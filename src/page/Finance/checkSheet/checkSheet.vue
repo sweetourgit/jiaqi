139d84e80651afcb1fea2a12815869aa89c3db4a
wzj <1063671457@qq.com>
2020-03-30 16:41:35 +0800

    招商银行字段 跟团游回退版本

--- src/page/Finance/checkSheet/checkSheet.vue
+++ src/page/Finance/checkSheet/checkSheet.vue
@@ -1,18 +1,26 @@
 <template>
-  <div id="outDiv" :style="{'minHeight':height+'px'}">
-    <el-container style="height:100%;">
+<!--   //跟团游 去掉待审批 -->
+  <!-- <div id="outDiv" :style="{'minHeight':height+'px'}"> -->
+  <!-- <el-container style="height:100%;">
       <el-header>
         <el-tabs v-model="activeName" @tab-click="handleClick">
           <el-tab-pane label="跟团游" name="team"></el-tab-pane>
           <el-tab-pane label="周边游" name="around"></el-tab-pane>
         </el-tabs>
-      </el-header>
-      <!-- 只有周边游组件下会生成报账单 需要您审批 -->
-      <AroundLayout v-show="activeName=='around'" :parentHeight="height"></AroundLayout>
+  </el-header>-->
+  <!-- 只有周边游组件下会生成报账单 需要您审批 -->
+  <!-- <AroundLayout v-show="activeName=='around'" :parentHeight="height"></AroundLayout>
       <el-main v-show="activeName!='around'">
         <router-view></router-view>
       </el-main>
-    </el-container>
+  </el-container>-->
+  <!-- </div> -->
+  <div style="padding-bottom: 80px;">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="跟团游" name="team"></el-tab-pane>
+      <el-tab-pane label="周边游" name="around"></el-tab-pane>
+    </el-tabs>
+    <router-view></router-view>
   </div>
 </template>
 <script>
@@ -28,8 +36,9 @@ export default {
     };
   },
   mounted() {
-    this.height =
-      document.getElementById("outDiv").parentElement.offsetHeight - 30;
+    //跟团游 去掉待审批
+    // this.height =
+    //   document.getElementById("outDiv").parentElement.offsetHeight - 30;
     this.handleClick({ name: "team" });
   },
   methods: {
--- src/page/Finance/checkSheet/team/team.vue
+++ src/page/Finance/checkSheet/team/team.vue
@@ -1,17 +1,29 @@
 <template>
-  <div class="check-sheet">
+<!--   //跟团游 去掉待审批 -->
+  <!-- <div class="check-sheet">
     <all-pane ref="allPaneRef"></all-pane>
+  </div>-->
+  <div class="check-sheet">
+    <el-tabs v-model="currentTab" @tab-click="tabClickHandler">
+      <el-tab-pane label="报账单" name="all">
+        <all-pane ref="allPaneRef"></all-pane>
+      </el-tab-pane>
+      <el-tab-pane :label="'需要您审批（'+ mineCount +'）'" name="mine">
+        <mine-pane ref="minePaneRef" @mine-count="mineCount= $event"></mine-pane>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
 import { getCheckSheetList, getFlowModel } from "./api";
 import allPane from "./comps/allPane/allPane";
-// import minePane from "./comps/minePane/minePane";
+import minePane from "./comps/minePane/minePane";
 
 export default {
-  // components: { allPane, minePane },
-   components: { allPane},
+  components: { allPane, minePane },
+    //跟团游 去掉待审批
+  // components: { allPane },
 
   mounted() {
     this.init();
@@ -39,7 +51,7 @@ export default {
       let { tab, conditions } = query || {};
       if (tab) this.currentTab = tab;
       this.$refs.allPaneRef.init(tab === "all" && conditions);
-      // this.$refs.minePaneRef.init();
+      this.$refs.minePaneRef.init();
     },
 
     // 分流query
