300c4961d169c14fa65107b6b78842d83f6ca983
Gao Yun <67802052@qq.com>
2020-03-31 15:11:34 +0800

    更换报账单审批流程接口

--- src/page/Finance/checkSheetDetail/subs/team/comps/printGround/subs/review.vue
+++ src/page/Finance/checkSheetDetail/subs/team/comps/printGround/subs/review.vue
@@ -12,7 +12,7 @@
         <el-table-column prop="finishedTime" label="审批时间" width="150" align="center"></el-table-column>
         <el-table-column prop="participantName" label="审批人" align="center"></el-table-column>
         <el-table-column prop="approvalName" label="审批结果" align="center"></el-table-column>
-        <el-table-column prop="No" label="审批意见" align="center"></el-table-column>
+        <el-table-column prop="comments" label="审批意见" align="center"></el-table-column>
       </el-table>
     <!-- 审核结果 END -->
   </div>
