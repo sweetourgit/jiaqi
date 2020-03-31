8a4fe544e9efcd40dc84238871c846c7ee5db6e1
Gao Yun <67802052@qq.com>
2020-03-31 15:01:30 +0800

    更换报账单审批流程接口

--- src/page/Finance/checkSheetDetail/subs/team/comps/printGround/subs/review.vue
+++ src/page/Finance/checkSheetDetail/subs/team/comps/printGround/subs/review.vue
@@ -1,6 +1,6 @@
 /*
- * @Author: WZJ 
- * @Date: 2020-03-27 09:11:22 
+ * @Author: WZJ
+ * @Date: 2020-03-27 09:11:22
  * @Last Modified by: WZJ
  * @Last Modified time: 2020-03-30 09:48:04
  */
@@ -9,10 +9,10 @@
     <!-- 审核结果 -->
     <el-divider content-position="left" class='title-margin title-margin-t'>审核结果</el-divider>
       <el-table :data="reviewList" border >
-      <el-table-column prop="participantName" label="审批人" align="center"></el-table-column>
-      <el-table-column prop="approvalName" label="审批结果" align="center"></el-table-column>
-      <el-table-column prop="No" label="审批意见" align="center"></el-table-column>
-      <el-table-column prop="finishedTime" label="审批时间" align="center"></el-table-column>
+        <el-table-column prop="finishedTime" label="审批时间" width="150" align="center"></el-table-column>
+        <el-table-column prop="participantName" label="审批人" align="center"></el-table-column>
+        <el-table-column prop="approvalName" label="审批结果" align="center"></el-table-column>
+        <el-table-column prop="No" label="审批意见" align="center"></el-table-column>
       </el-table>
     <!-- 审核结果 END -->
   </div>
